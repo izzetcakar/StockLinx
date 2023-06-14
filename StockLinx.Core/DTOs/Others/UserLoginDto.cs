3fabab6775f29085c670cf370caac137d2661f25
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-14 10:18:01 +0300

    Update UserLoginDto.cs

--- StockLinx.Core/DTOs/Others/UserLoginDto.cs
+++ StockLinx.Core/DTOs/Others/UserLoginDto.cs
@@ -1,6 +1,27 @@
-﻿namespace StockLinx.Core.DTOs.Others
+﻿using StockLinx.Core.Entities;
+using StockLinx.Core.Models;
+
+namespace StockLinx.Core.DTOs.Others
 {
     public class UserLoginDto
     {
+        public Guid? ImageId { get; set; }
+        public Guid? CompanyId { get; set; }
+        public Guid? ManagerId { get; set; }
+        public Guid? DepartmentId { get; set; }
+        public Guid? LocationId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string? AdminLvl { get; set; }
+        public string? Language { get; set; }
+        public string EmployeeNo { get; set; }
+        public string? JobTitle { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string? PhoneNo { get; set; }
+        public string? Website { get; set; }
+        public string? Notes { get; set; }
     }
 }
