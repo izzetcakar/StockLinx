50208f5a066d2d8415cecd33573a64a81154e151
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-14 10:19:08 +0300

    Update UserService.cs

--- StockLinx.Service/Services/UserService.cs
+++ StockLinx.Service/Services/UserService.cs
@@ -1,6 +1,55 @@
-﻿namespace StockLinx.Service.Services
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using StockLinx.Core.DTOs.Others;
+using StockLinx.Core.Models;
+using StockLinx.Core.Repositories;
+using StockLinx.Core.Services;
+using StockLinx.Core.UnitOfWork;
+using System.Security.Claims;
+
+namespace StockLinx.Service.Services
 {
-    internal class UserService
+    public class UserService : Service<User>, IUserService
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IRepository<User> repository, IUnitOfWork unitOfWork,
+            IUserRepository userRepository, IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(repository, unitOfWork)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetMyName()
+        {
+            var result = string.Empty;
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                result = _httpContextAccessor.HttpContext.User.FindFirstValue("UserId");
+                return result;
+            }
+            else
+            {
+                throw new NotImplementedException("Invalid Token");
+            }
+        }
+
+        public async Task<User> Login(UserLoginDto userLoginDto)
+        {
+            return await _userRepository.Login(userLoginDto);
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user)
+        {
+            return await _userRepository.Register(user);
+        }
     }
 }
