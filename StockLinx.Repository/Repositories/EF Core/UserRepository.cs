396d41fe78c90b3f3d457d4c34db7851fbb613fc
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-14 10:18:06 +0300

    Update UserRepository.cs

--- StockLinx.Repository/Repositories/EF Core/UserRepository.cs
+++ StockLinx.Repository/Repositories/EF Core/UserRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using StockLinx.Core.DTOs.Others;
+using StockLinx.Core.Models;
+using StockLinx.Core.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,55 @@ using System.Threading.Tasks;
 
 namespace StockLinx.Repository.Repositories.EF_Core
 {
-    internal class UserRepository
+    public class UserRepository : Repository<User>, IUserRepository
     {
+        private readonly IMapper _mapper;
+        public UserRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+
+        public string GetMyName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Login(UserLoginDto userLoginDto)
+        {
+            var isExist = await dbContext.Users.AnyAsync(x => x.Email == userLoginDto.Email && x.Password == userLoginDto.Password);
+            if (!isExist)
+            {
+                throw new Exception("User is not found");
+            }
+            else
+            {
+                return await dbContext.Users.Where(x => x.Email == userLoginDto.Email && x.Password == userLoginDto.Password).SingleOrDefaultAsync();
+            }
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user)
+        {
+            var nameExist = await dbContext.Users.AnyAsync(x => x.Email == user.Email);
+            var emailExist = await dbContext.Users.AnyAsync(x => x.Email == user.Email);
+
+            if (nameExist)
+            {
+                throw new Exception("Username already exists");
+            }
+            else if (emailExist)
+            {
+                throw new Exception("Email already exists");
+            }
+            else
+            {
+                await dbContext.Users.AddAsync(user);
+                await dbContext.SaveChangesAsync();
+                return await dbContext.Users.Where(x => x.Email == user.Email).SingleOrDefaultAsync();
+            }
+        }
     }
 }
