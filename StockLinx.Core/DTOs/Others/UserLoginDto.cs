5fbebf2f027992ba3c1b3bc82157723744b22434
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-14 09:51:27 +0300

    Repository Interfaces Created

---
+++ StockLinx.Core/DTOs/Others/UserLoginDto.cs
@@ -0,0 +1,6 @@
+﻿namespace StockLinx.Core.DTOs.Others
+{
+    public class UserLoginDto
+    {
+    }
+}
---
+++ StockLinx.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using StockLinx.Core.DTOs.Others;
+using StockLinx.Core.Models;
+
+namespace StockLinx.Core.Repositories
+{
+
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> Login(UserLoginDto userLoginDto);
+        Task Logout();
+        Task<User> Register(User user);
+        string GetMyName();
+    }
+
+
+}
