b6b251fc78610c5866213d8f7ac3dc8acb2732cf
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-15 10:40:29 +0300

    Dto and entity classes are mapped

--- StockLinx.Service/Mapping/MapProfile.cs
+++ StockLinx.Service/Mapping/MapProfile.cs
@@ -1,8 +1,30 @@
 ﻿using AutoMapper;
+using StockLinx.Core.DTOs.Generic;
+using StockLinx.Core.Entities;
+using StockLinx.Core.Models;
 
 namespace StockLinx.Service.Mapping
 {
     public class MapProfile : Profile
     {
+        public MapProfile()
+        {
+            CreateMap<Accessory, AccessoryDto>().ReverseMap();
+            CreateMap<Asset, AssetDto>().ReverseMap();
+            CreateMap<BaseProduct, BaseProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<Component, ComponentDto>().ReverseMap();
+            CreateMap<Consumable, ConsumableDto>().ReverseMap();
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap<DeployedProduct, DepartmentDto>().ReverseMap();
+            CreateMap<Image, ImageDto>().ReverseMap();
+            CreateMap<License, LicenseDto>().ReverseMap();
+            CreateMap<Location, LocationDto>().ReverseMap();
+            CreateMap<Manufacturer, ManufacturerDto>().ReverseMap();
+            CreateMap<Model, ModelDto>().ReverseMap();
+            CreateMap<Supplier, SupplierDto>().ReverseMap();
+            CreateMap<User, UserDto>().ReverseMap();
+        }
     }
 }
