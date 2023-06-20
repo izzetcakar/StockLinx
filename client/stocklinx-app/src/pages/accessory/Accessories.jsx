3bc57f63c50be8d1073ecacd28ec838ccb144c97
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-20 11:01:26 +0300

    Base components created

---
+++ client/stocklinx-app/src/pages/accessory/Accessories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Accessories = () => {
+  return <div></div>;
+};
+
+export default Accessories;
---
+++ client/stocklinx-app/src/pages/people/Department.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Department = () => {
+  return <div></div>;
+};
+
+export default Department;
