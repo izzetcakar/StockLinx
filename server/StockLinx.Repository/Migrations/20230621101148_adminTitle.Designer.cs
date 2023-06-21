29447cca86240aff2518373a54f69c6767e4ffb7
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-21 16:35:32 +0300

    Db Context Updated

--- server/StockLinx.Repository/Migrations/20230621101148_adminTitle.Designer.cs
+++ server/StockLinx.Repository/Migrations/20230621101148_adminTitle.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using StockLinx.Repository;
@@ -11,9 +12,11 @@ using StockLinx.Repository;
 namespace StockLinx.Repository.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230621101148_adminTitle")]
+    partial class adminTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -872,7 +875,7 @@ namespace StockLinx.Repository.Migrations
                     b.Property<string>("PhoneNo")
                         .HasColumnType("text");
 
-                    b.Property<DateTime?>("StartDate")
+                    b.Property<DateTime>("StartDate")
                         .HasColumnType("timestamp with time zone");
 
                     b.Property<DateTime?>("UpdatedDate")
