bd4fbaf5ed8400a0c3e9455e0b561db6805fcd24
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-21 16:34:58 +0300

    Update IService.cs

--- server/StockLinx.Core/Services/IService.cs
+++ server/StockLinx.Core/Services/IService.cs
@@ -8,8 +8,8 @@ namespace StockLinx.Core.Services
         Task<IEnumerable<T>> GetAllAsync();
         Task<IEnumerable<T>> Where(Expression<Func<T, bool>> expression);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task<T> AddAsync(T entity);
-        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T oldEntity, T newEntity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
         Task RemoveAsync(T entity);
