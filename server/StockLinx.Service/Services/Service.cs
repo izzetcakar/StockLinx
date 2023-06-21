571d4c112e3e15705ae23aaa32f514ca418ba25f
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-21 16:36:45 +0300

    Update Service.cs

--- server/StockLinx.Service/Services/Service.cs
+++ server/StockLinx.Service/Services/Service.cs
@@ -17,18 +17,16 @@ namespace StockLinx.Service.Services
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<T> AddAsync(T entity)
+        public async Task AddAsync(T entity)
         {
             await _repository.AddAsync(entity);
             await _unitOfWork.CommitAsync();
-            return entity;
         }
 
-        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        public async Task AddRangeAsync(IEnumerable<T> entities)
         {
             await _repository.AddRangeAsync(entities);
             await _unitOfWork.CommitAsync();
-            return entities;
         }
 
         public async Task<bool> AnyAsync(Expression<Func<T, bool>> expression)
