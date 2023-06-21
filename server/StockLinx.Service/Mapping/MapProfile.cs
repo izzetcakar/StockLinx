24078c57acdadd1b3e6c665500d6b557d792c52b
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-21 16:35:37 +0300

    Update MapProfile.cs

--- server/StockLinx.Service/Mapping/MapProfile.cs
+++ server/StockLinx.Service/Mapping/MapProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using StockLinx.Core.DTOs.Create;
 using StockLinx.Core.DTOs.Generic;
 using StockLinx.Core.Entities;
 
@@ -9,21 +10,37 @@ namespace StockLinx.Service.Mapping
         public MapProfile()
         {
             CreateMap<Accessory, AccessoryDto>().ReverseMap();
+            CreateMap<Accessory, AccessoryCreateDto>().ReverseMap();
             CreateMap<Asset, AssetDto>().ReverseMap();
+            CreateMap<Asset, AssetCreateDto>().ReverseMap();
             CreateMap<BaseProduct, BaseProductDto>().ReverseMap();
+            CreateMap<BaseProduct, BaseProductCreateDto>().ReverseMap();
             CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateDto>().ReverseMap();
             CreateMap<Company, CompanyDto>().ReverseMap();
+            CreateMap<Company, CompanyCreateDto>().ReverseMap();
             CreateMap<Component, ComponentDto>().ReverseMap();
+            CreateMap<Component, ComponentCreateDto>().ReverseMap();
             CreateMap<Consumable, ConsumableDto>().ReverseMap();
+            CreateMap<Consumable, ConsumableCreateDto>().ReverseMap();
             CreateMap<Department, DepartmentDto>().ReverseMap();
-            CreateMap<DeployedProduct, DepartmentDto>().ReverseMap();
+            CreateMap<Department, DepartmentCreateDto>().ReverseMap();
+            CreateMap<DeployedProduct, DeployedProductDto>().ReverseMap();
+            CreateMap<DeployedProduct, DeployedProductCreateDto>().ReverseMap();
             CreateMap<Image, ImageDto>().ReverseMap();
+            CreateMap<Image, ImageCreateDto>().ReverseMap();
             CreateMap<License, LicenseDto>().ReverseMap();
+            CreateMap<License, LicenseCreateDto>().ReverseMap();
             CreateMap<Location, LocationDto>().ReverseMap();
+            CreateMap<Location, LocationCreateDto>().ReverseMap();
             CreateMap<Manufacturer, ManufacturerDto>().ReverseMap();
+            CreateMap<Manufacturer, ManufacturerCreateDto>().ReverseMap();
             CreateMap<Model, ModelDto>().ReverseMap();
+            CreateMap<Model, ModelCreateDto>().ReverseMap();
             CreateMap<Supplier, SupplierDto>().ReverseMap();
+            CreateMap<Supplier, SupplierCreateDto>().ReverseMap();
             CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<User, UserCreateDto>().ReverseMap();
         }
     }
 }
