12529061326a581b0ab77def19d1b3a68f1574b7
İzzet Çakar <izzetcakar4@gmail.com>
2023-06-23 10:18:53 +0300

    Create EnumHandler.cs

---
+++ server/StockLinx.Core/Entities/EnumHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace StockLinx.Core.Entities
+{
+    public enum Status
+    {
+        Deployable,
+        Deployed
+    }
+    public enum CategoryType
+    {
+        Asset,
+        Accessory,
+        Component,
+        Consumable
+    }
+    public class EnumHandler
+    {
+
+    }
+}
