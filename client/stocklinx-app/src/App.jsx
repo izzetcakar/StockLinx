b26b0f79e9d0bf59814e36001d52ba6595cb6eb2
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-04 20:28:00 +0300

    .

--- client/stocklinx-app/src/App.jsx
+++ client/stocklinx-app/src/App.jsx
@@ -14,8 +14,18 @@ import { useSelector } from "react-redux";
 import { checkEmpty } from "./functions/checkEmpty";
 
 function App() {
-  const userRedux = useSelector((state) => state.user.user);
+  // const userRedux = useSelector((state) => state.user.user);
   const { loading } = useAuth();
+  const userRedux = {
+    createdDate: "2023-06-23T10:26:20.567788Z",
+    email: "admin@gmail.com",
+    employeeNo: "1",
+    firstName: "adminF",
+    id: "b4701e6b-7170-4504-ae3a-999e40c28fe3",
+    isAdmin: false,
+    lastName: "adminL",
+    password: "123",
+  };
 
   if (loading) {
     return <LoadPanel visible={true} />;
--- client/stocklinx-app/src/layouts/side-nav-outer-toolbar/side-nav-outer-toolbar.jsx
+++ client/stocklinx-app/src/layouts/side-nav-outer-toolbar/side-nav-outer-toolbar.jsx
@@ -7,8 +7,41 @@ import "./side-nav-outer-toolbar.scss";
 import { useScreenSize } from "../../utils/media-query";
 import { Template } from "devextreme-react/core/template";
 import { useMenuPatch } from "../../utils/patches";
+import "boxicons";
+import logo from "../../images/logo.png";
 
 export default function SideNavOuterToolbar({ title, children }) {
+  const [navigationList, setNavigationList] = useState([
+    {
+      title: "Home",
+      icon: "home-alt",
+      color: "#737373",
+      elements: [{ title: "Test", icon: "bar-chart-alt-2", color: "#737373" }],
+      displayElements: false,
+    },
+    {
+      title: "Renevue",
+      icon: "bar-chart-alt-2",
+      color: "#737373",
+      elements: [{ title: "Test2", icon: "bar-chart-alt-2", color: "#737373" }],
+      displayElements: false,
+    },
+    {
+      title: "Notification",
+      icon: "bell",
+      color: "#737373",
+    },
+    {
+      title: "Analytics",
+      icon: "pie-chart-alt",
+      color: "#737373",
+    },
+    {
+      title: "Logout",
+      icon: "log-out",
+      color: "#737373",
+    },
+  ]);
   const scrollViewRef = useRef(null);
   const navigate = useNavigate();
   const { isXSmall, isLarge } = useScreenSize();
@@ -61,10 +94,30 @@ export default function SideNavOuterToolbar({ title, children }) {
     [navigate, menuStatus, isLarge]
   );
 
+  const handleNavElements = (target) => {
+    let newList = navigationList.map((item, index) => {
+      if (item?.elements && target === index) {
+        return { ...item, displayElements: !item.displayElements };
+      } else {
+        return item;
+      }
+    });
+    setNavigationList(newList);
+  };
+
+  const navigateUser = (item, index) => {
+    if (item?.elements) {
+      handleNavElements(index);
+    } else {
+      handleNavElements(-1);
+      //Navigate User
+    }
+  };
+
   return (
     <div className={"side-nav-outer-toolbar"}>
       <Header menuToggleEnabled toggleMenu={toggleMenu} title={title} />
-      <Drawer
+      {/* <Drawer
         className={["drawer", patchCssClass].join(" ")}
         position={"before"}
         closeOnOutsideClick={onOutsideClick}
@@ -98,11 +151,56 @@ export default function SideNavOuterToolbar({ title, children }) {
             onMenuReady={onMenuReady}
           ></SideNavigationMenu>
         </Template>
-      </Drawer>
+      </Drawer> */}
+      <div className="sidebar-container">
+        <div className="navigation-item">
+          <img src={logo} className="icon" />
+          <div className="title">Stocklinx</div>
+        </div>
+        {navigationList.map((item, index) => (
+          <React.Fragment key={index}>
+            <div
+              className="navigation-item"
+              onClick={() => navigateUser(item, index)}
+            >
+              <div className="icon">
+                <box-icon name={item.icon} size="1.4rem" color={item.color} />
+              </div>
+              <div className="title">{item.title}</div>
+              {item?.elements ? (
+                <div
+                  className={
+                    item?.displayElements ? "arrow-down-open" : "arrow-down"
+                  }
+                >
+                  <box-icon name="chevron-down" size="1.4rem" color="#737373" />
+                </div>
+              ) : null}
+            </div>
+            {item?.displayElements ? (
+              item.elements.map((el, nestedIndex) => (
+                <div
+                  className="navigation-element"
+                  key={`${index}-${nestedIndex}`}
+                >
+                  <div className="icon">
+                    <box-icon name={el.icon} size="1.4rem" color={el.color} />
+                  </div>
+                  <div className="title">{el.title}</div>
+                </div>
+              ))
+            ) : (
+              <></>
+            )}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
-
+{
+  /* <box-icon name='search'></box-icon> */
+}
 const MenuStatus = {
   Closed: 1,
   Opened: 2,
