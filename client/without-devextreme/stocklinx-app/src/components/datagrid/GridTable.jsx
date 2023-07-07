6a3a3bb5e360ce81287279d19f23a7ef284b18f8
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-07 16:45:07 +0300

    Update GridTable.jsx

--- client/without-devextreme/stocklinx-app/src/components/datagrid/GridTable.jsx
+++ client/without-devextreme/stocklinx-app/src/components/datagrid/GridTable.jsx
@@ -1,37 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./gridTable.scss";
 import { checkEmpty } from "../../functions/checkEmpty";
+import SelectCheckbox from "../select/SelectCheckbox";
 
-const Column = ({ dataField, caption, dataType }) => {
-  return <div className="property">{caption}</div>;
-};
-const EditRenderComponent = () => {
-  return (
-    <div className="edit-container">
-      <div className="element">
-        <box-icon type="solid" name="edit-alt" size="1.5rem"></box-icon>
+const GridTable = ({ data, columns, hasColumnLines }) => {
+  const [propertyDataStyle, setPropertyDataStyle] = useState({});
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+
+  useEffect(() => {
+    handleTableStyle();
+  }, [hasColumnLines]);
+
+  const Column = ({ dataField, caption, dataType }) => {
+    return (
+      <div className="property">
+        {dataField === "Select" ? (
+          <SelectCheckbox
+            selectFunc={() => handleSelectAll()}
+            selectId={"ChangeAll"}
+            isChecked={checkEmpty(selectedIndexes)}
+          />
+        ) : (
+          caption
+        )}
       </div>
-      <div className="element">
-        <box-icon type="solid" name="x-square" size="1.5rem"></box-icon>
+    );
+  };
+
+  const EditObjectComponent = (rowIndex) => {
+    return (
+      <div className="edit-container">
+        <div className="element" onClick={() => console.log(data[rowIndex])}>
+          <box-icon type="solid" name="edit-alt" size="1.5rem"></box-icon>
+        </div>
+        <div className="element" onClick={() => console.log(rowIndex)}>
+          <box-icon type="solid" name="x-square" size="1.5rem"></box-icon>
+        </div>
       </div>
-    </div>
-  );
-};
-const GridTable = ({ data, columns }) => {
+    );
+  };
+  const SelectObjectComponent = (rowIndex) => {
+    return (
+      <SelectCheckbox
+        selectFunc={() => selectObject(rowIndex)}
+        selectId={rowIndex}
+        isChecked={selectedIndexes.includes(rowIndex)}
+      />
+    );
+  };
+  const selectObject = (index) => {
+    setSelectedIndexes((prevIndexes) => {
+      if (prevIndexes.includes(index)) {
+        return prevIndexes.filter((i) => i !== index);
+      } else {
+        return [...prevIndexes, index];
+      }
+    });
+  };
+  const handleSelectAll = () => {
+    if (checkEmpty(selectedIndexes)) {
+      setSelectedIndexes([]);
+    } else {
+      setSelectedIndexes(columns.map((_, index) => index));
+    }
+  };
   const renderValue = (rowIndex, column) => {
+    const value = data[rowIndex][column.dataField];
+
+    if (column.dataField === "Edit") {
+      return EditObjectComponent(rowIndex);
+    }
+    if (column.dataField === "Select") {
+      return SelectObjectComponent(rowIndex);
+    }
     if (column.renderComponent) {
-      const value = data[rowIndex][column.dataField];
       return column.renderComponent(value);
     }
-    const value = data[rowIndex][column.dataField];
     if (value === null || value === undefined) {
       return "";
     }
     if (typeof value === "boolean") {
-      let name = value ? "check" : "x";
-      let color = value ? "#63bd4f" : "#ed6b6b";
+      const name = value ? "check" : "x";
+      const color = value ? "#63bd4f" : "#ed6b6b";
       return <box-icon size="1.5rem" name={name} color={color}></box-icon>;
     }
+
     return value;
   };
   const ColumnsEmptyHandler = (columns, data) => {
@@ -42,26 +95,49 @@ const GridTable = ({ data, columns }) => {
         dataType: (typeof dataField).toString(),
         renderComponent: null,
       }));
-      return AddEditToColumns(newColumns);
+      return AddToolComponents(newColumns);
     }
-    return AddEditToColumns(columns);
+    return AddToolComponents(columns);
   };
-  const AddEditToColumns = (columns) => {
+  const AddToolComponents = (columns) => {
     return [
+      {
+        dataField: "Select",
+        caption: "",
+        dataType: "action",
+        renderComponent: SelectObjectComponent,
+      },
       ...columns,
       {
         dataField: "Edit",
         caption: "",
         dataType: "action",
-        renderComponent: EditRenderComponent,
+        renderComponent: EditObjectComponent,
       },
     ];
   };
+
+  const handleTableStyle = () => {
+    const style = {};
+    if (hasColumnLines) {
+      style.borderRight = "1px solid #ccc";
+    }
+    setPropertyDataStyle(style);
+  };
+
   return (
     <div className="table-container">
       <div className="columns-data-container">
         {ColumnsEmptyHandler(columns, data).map((column, columnIndex) => (
-          <div className="property-data" key={columnIndex}>
+          <div
+            className="property-data"
+            key={columnIndex}
+            style={
+              column.dataField === "Edit" && hasColumnLines
+                ? { ...propertyDataStyle, borderLeft: "0" }
+                : propertyDataStyle
+            }
+          >
             <Column {...column} />
             {data.map((_, rowIndex) => (
               <div className="data-value" key={rowIndex}>
