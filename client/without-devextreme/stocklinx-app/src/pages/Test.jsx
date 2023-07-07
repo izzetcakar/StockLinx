cf254d927f6e78f97035ae87bd1446a7e4ac7550
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-07 12:55:11 +0300

    Update Test.jsx

--- client/without-devextreme/stocklinx-app/src/pages/Test.jsx
+++ client/without-devextreme/stocklinx-app/src/pages/Test.jsx
@@ -1,22 +1,7 @@
 import React from "react";
 import "./test.scss";
-import { checkEmpty } from "../functions/checkEmpty";
+import GridTable from "../components/datagrid/GridTable";
 
-const Column = ({ dataField, caption, dataType }) => {
-  return <div className="property">{caption}</div>;
-};
-const EditRenderComponent = () => {
-  return (
-    <div className="edit-container">
-      <div className="element">
-        <box-icon type="solid" name="edit-alt" size="1.5rem"></box-icon>
-      </div>
-      <div className="element">
-        <box-icon type="solid" name="x-square" size="1.5rem"></box-icon>
-      </div>
-    </div>
-  );
-};
 function titleComponent(value) {
   return <div style={{ fontWeight: "bold" }}>{value}</div>;
 }
@@ -56,12 +41,6 @@ const Test = () => {
       dataType: "number",
       renderComponent: appComponent,
     },
-    {
-      dataField: "Edit",
-      caption: "",
-      dataType: "action",
-      renderComponent: EditRenderComponent,
-    },
   ];
 
   const data = [
@@ -94,51 +73,11 @@ const Test = () => {
     },
   ];
 
-  const renderValue = (rowIndex, column) => {
-    if (column.renderComponent) {
-      const value = data[rowIndex][column.dataField];
-      return column.renderComponent(value);
-    }
-    const value = data[rowIndex][column.dataField];
-    if (value === null || value === undefined) {
-      return "";
-    }
-    if (typeof value === "boolean") {
-      let name = value ? "check" : "x";
-      let color = value ? "#63bd4f" : "#ed6b6b";
-      return <box-icon size="1.5rem" name={name} color={color}></box-icon>;
-    }
-    return value;
-  };
-  const ColumnsEmptyHandler = (columns, data) => {
-    if (!checkEmpty(columns) && checkEmpty(data)) {
-      return Object.keys(data[0]).map((dataField) => ({
-        dataField,
-        caption: dataField,
-        dataType: (typeof dataField).toString(),
-        renderComponent: null,
-      }));
-    }
-    return columns;
-  };
   return (
     <div className="page-data">
       <div className="title">Computers</div>
       <div className="description">Description</div>
-      <div className="table-container">
-        <div className="columns-data-container">
-          {ColumnsEmptyHandler(columns, data).map((column, columnIndex) => (
-            <div className="property-data" key={columnIndex}>
-              <Column {...column} />
-              {data.map((_, rowIndex) => (
-                <div className="data-value" key={rowIndex}>
-                  {renderValue(rowIndex, column)}
-                </div>
-              ))}
-            </div>
-          ))}
-        </div>
-      </div>
+      <GridTable data={data} columns={columns} />
     </div>
   );
 };
