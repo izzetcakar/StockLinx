7ccb954fb85ab5e503ee2f53eac9d8174e0510a6
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-10 14:16:49 +0300

    Update AccessoryUpdateDto.cs

--- server/StockLinx.Core/DTOs/Update/AccessoryUpdateDto.cs
+++ server/StockLinx.Core/DTOs/Update/AccessoryUpdateDto.cs
@@ -1,9 +1,12 @@
-﻿namespace StockLinx.Core.DTOs.Update
+﻿using StockLinx.Core.DTOs.Create;
+
+namespace StockLinx.Core.DTOs.Update
 {
     public class AccessoryUpdateDto : BaseProductUpdateDto
     {
         public Guid? ManufacturerId { get; set; }
         public Guid? SupplierId { get; set; }
         public int? Warranty { get; set; }
+        public ImageCreateDto? Image { get; set; }
     }
 }
