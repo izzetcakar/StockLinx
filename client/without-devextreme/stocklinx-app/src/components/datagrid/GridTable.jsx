a62b5790aebc5d1f297d1caece0b0f6cb145a97e
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-10 16:16:44 +0300

    Update GridTable.jsx

--- client/without-devextreme/stocklinx-app/src/components/datagrid/GridTable.jsx
+++ client/without-devextreme/stocklinx-app/src/components/datagrid/GridTable.jsx
@@ -2,35 +2,40 @@ import React, { useState, useEffect } from "react";
 import "./gridTable.scss";
 import { checkEmpty } from "../../functions/checkEmpty";
 import SelectCheckbox from "../select/SelectCheckbox";
+import { hasAllElements } from "../../functions/hasAllElements";
+import { deepCopy } from "../../functions/deepCopy";
+import { getIndexesFromArray } from "../../functions/getIndexesFromArray";
 
-const GridTable = ({ data, columns, hasColumnLines }) => {
+const pageSizes = [1, 3, 5];
+
+const GridTable = ({
+  data,
+  columns,
+  hasColumnLines = false,
+  showPageSizeSelector = true,
+  showPageSizeInfo = true,
+  showPageSize = true,
+}) => {
+  const [gridData, setGridData] = useState([]);
   const [propertyDataStyle, setPropertyDataStyle] = useState({});
   const [selectedIndexes, setSelectedIndexes] = useState([]);
+  const [selectedPageSize, setSelectedPageSize] = useState(pageSizes[0]);
+
+  useEffect(() => {
+    setGridData(deepCopy(data));
+  }, [data]);
 
   useEffect(() => {
     handleTableStyle();
   }, [hasColumnLines]);
 
-  const Column = ({ dataField, caption, dataType }) => {
-    return (
-      <div className="property">
-        {dataField === "Select" ? (
-          <SelectCheckbox
-            selectFunc={() => handleSelectAll()}
-            selectId={"ChangeAll"}
-            isChecked={checkEmpty(selectedIndexes)}
-          />
-        ) : (
-          caption
-        )}
-      </div>
-    );
-  };
-
-  const EditObjectComponent = (rowIndex) => {
+  const EditObjectComponent = ({ rowIndex }) => {
     return (
       <div className="edit-container">
-        <div className="element" onClick={() => console.log(data[rowIndex])}>
+        <div
+          className="element"
+          onClick={() => console.log(gridData[rowIndex])}
+        >
           <box-icon type="solid" name="edit-alt" size="1.5rem" />
         </div>
         <div className="element" onClick={() => console.log(rowIndex)}>
@@ -39,14 +44,69 @@ const GridTable = ({ data, columns, hasColumnLines }) => {
       </div>
     );
   };
-  const SelectObjectComponent = (rowIndex) => {
+  const SelectObjectComponent = ({ rowIndex, isChecked, selectFunc }) => {
     return (
       <SelectCheckbox
-        selectFunc={() => selectObject(rowIndex)}
+        selectFunc={() => selectFunc(rowIndex)}
         selectId={rowIndex}
-        isChecked={selectedIndexes.includes(rowIndex)}
+        isChecked={isChecked}
       />
     );
+  };
+  const PageSizeComponent = () => {
+    if (!showPageSizeSelector && !showPageSizeInfo) {
+      return null;
+    }
+    return (
+      <div className="page-size-container">
+        {showPageSizeSelector ? (
+          <div className="size-container">
+            {pageSizes.map((item) => (
+              <div
+                className={`size ${
+                  item === selectedPageSize ? "selected" : ""
+                }`}
+                key={item}
+                onClick={() => setSelectedPageSize(item)}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        ) : null}
+
+        {showPageSizeInfo ? (
+          <div className="selected-count-container">
+            Page {selectedIndexes.length} of{" "}
+            {Math.min(selectedPageSize, gridData.length)} (
+            {selectedIndexes.length} items)
+          </div>
+        ) : null}
+      </div>
+    );
+  };
+  const addToolComponents = (columns) => {
+    return [
+      {
+        dataField: "Select",
+        caption: "",
+        dataType: "action",
+        renderComponent: (rowIndex) => (
+          <SelectObjectComponent
+            rowIndex={rowIndex}
+            isChecked={selectedIndexes.includes(rowIndex)}
+            selectFunc={selectObject}
+          />
+        ),
+      },
+      ...columns,
+      {
+        dataField: "Edit",
+        caption: "",
+        dataType: "action",
+        renderComponent: EditObjectComponent,
+      },
+    ];
   };
   const selectObject = (index) => {
     setSelectedIndexes((prevIndexes) => {
@@ -58,27 +118,37 @@ const GridTable = ({ data, columns, hasColumnLines }) => {
     });
   };
   const handleSelectAll = () => {
-    if (checkEmpty(selectedIndexes)) {
+    if (hasAllElements(getIndexesFromArray(gridData), selectedIndexes)) {
       setSelectedIndexes([]);
     } else {
-      setSelectedIndexes(columns.map((_, index) => index));
+      setSelectedIndexes(getIndexesFromArray(gridData));
     }
   };
   const renderValue = (rowIndex, column) => {
-    const value = data[rowIndex][column.dataField];
+    const value = gridData[rowIndex][column.dataField];
 
     if (column.dataField === "Edit") {
-      return EditObjectComponent(rowIndex);
+      return <EditObjectComponent rowIndex={rowIndex} />;
     }
+
     if (column.dataField === "Select") {
-      return SelectObjectComponent(rowIndex);
+      return (
+        <SelectObjectComponent
+          rowIndex={rowIndex}
+          isChecked={selectedIndexes.includes(rowIndex)}
+          selectFunc={selectObject}
+        />
+      );
     }
+
     if (column.renderComponent) {
       return column.renderComponent(value);
     }
+
     if (value === null || value === undefined) {
       return "";
     }
+
     if (typeof value === "boolean") {
       const name = value ? "check" : "x";
       const color = value ? "#63bd4f" : "#ed6b6b";
@@ -87,47 +157,52 @@ const GridTable = ({ data, columns, hasColumnLines }) => {
 
     return value;
   };
-  const ColumnsEmptyHandler = (columns, data) => {
-    if (!checkEmpty(columns) && checkEmpty(data)) {
-      let newColumns = Object.keys(data[0]).map((dataField) => ({
+  const handleColumnsEmpty = (columns) => {
+    if (!checkEmpty(columns) && checkEmpty(gridData)) {
+      const newColumns = Object.keys(gridData[0]).map((dataField) => ({
         dataField,
         caption: dataField,
-        dataType: (typeof dataField).toString(),
+        dataType: typeof dataField,
         renderComponent: null,
       }));
-      return AddToolComponents(newColumns);
+
+      return addToolComponents(newColumns);
     }
-    return AddToolComponents(columns);
-  };
-  const AddToolComponents = (columns) => {
-    return [
-      {
-        dataField: "Select",
-        caption: "",
-        dataType: "action",
-        renderComponent: SelectObjectComponent,
-      },
-      ...columns,
-      {
-        dataField: "Edit",
-        caption: "",
-        dataType: "action",
-        renderComponent: EditObjectComponent,
-      },
-    ];
+
+    return addToolComponents(columns);
   };
   const handleTableStyle = () => {
-    const style = {};
-    if (hasColumnLines) {
-      style.borderRight = "1px solid #ccc";
-    }
+    const style = {
+      borderRight: hasColumnLines ? "1px solid #ccc" : "none",
+    };
     setPropertyDataStyle(style);
+  };
+  const filterData = () => {
+    return gridData.slice(0, selectedPageSize);
+  };
+  const GridColumn = ({ dataField, caption }) => {
+    if (dataField === "Select") {
+      return (
+        <div className="property">
+          <SelectCheckbox
+            selectFunc={handleSelectAll}
+            selectId="ChangeAll"
+            isChecked={hasAllElements(
+              getIndexesFromArray(gridData),
+              selectedIndexes
+            )}
+          />
+        </div>
+      );
+    }
+
+    return <div className="property">{caption}</div>;
   };
 
   return (
     <div className="table-container">
       <div className="columns-data-container">
-        {ColumnsEmptyHandler(columns, data).map((column, columnIndex) => (
+        {handleColumnsEmpty(columns).map((column, columnIndex) => (
           <div
             className="property-data"
             key={columnIndex}
@@ -137,8 +212,8 @@ const GridTable = ({ data, columns, hasColumnLines }) => {
                 : propertyDataStyle
             }
           >
-            <Column {...column} />
-            {data.map((_, rowIndex) => (
+            <GridColumn {...column} />
+            {filterData().map((_, rowIndex) => (
               <div className="data-value" key={rowIndex}>
                 {renderValue(rowIndex, column)}
               </div>
@@ -146,6 +221,7 @@ const GridTable = ({ data, columns, hasColumnLines }) => {
           </div>
         ))}
       </div>
+      {showPageSize ? <PageSizeComponent /> : null}
     </div>
   );
 };
