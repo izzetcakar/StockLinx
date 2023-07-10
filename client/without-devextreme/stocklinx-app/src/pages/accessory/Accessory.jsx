28c32d792dd2b2640c4b87eb8b025825a30d1857
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-10 17:41:44 +0300

    Update Accessory.jsx

--- client/without-devextreme/stocklinx-app/src/pages/accessory/Accessory.jsx
+++ client/without-devextreme/stocklinx-app/src/pages/accessory/Accessory.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "devextreme/data/odata/store";
 import { Workbook } from "exceljs";
 import { saveAs } from "file-saver-es";
@@ -24,6 +24,7 @@ import DataSource from "devextreme/data/data_source";
 import { Button } from "devextreme-react";
 import { notifyError } from "../../functions/notifyError";
 import { getAllProducts } from "../../redux/productReducer";
+import GridTable from "../../components/datagrid/GridTable";
 
 const Accessory = ({ Id }) => {
   const dispatch = useDispatch();
@@ -108,133 +109,266 @@ const Accessory = ({ Id }) => {
     selectedItemKeys.current = data.selectedRowKeys;
   };
 
+  const [options, setOptions] = useState([
+    {
+      option: "hasColumnLines",
+      value: false,
+    },
+    {
+      option: "showPageSize",
+      value: true,
+    },
+    {
+      option: "showPageSizeSelector",
+      value: true,
+    },
+    {
+      option: "showPageSizeInfo",
+      value: true,
+    },
+  ]);
+  const columns = [
+    {
+      dataField: "Owner",
+      caption: "Owner",
+      dataType: "string",
+      renderComponent: titleComponent,
+    },
+    { dataField: "LastCheck", caption: "Last Check", dataType: "string" },
+    { dataField: "OSversion", caption: "OS Version", dataType: "string" },
+    {
+      dataField: "PasswordManager",
+      caption: "Password Manager",
+      dataType: "boolean",
+    },
+    { dataField: "HDencrypted", caption: "HD Encrypted", dataType: "boolean" },
+    { dataField: "AVinstalled", caption: "AV Installed", dataType: "boolean" },
+    {
+      dataField: "Apllications",
+      caption: "Apllications",
+      dataType: "number",
+      renderComponent: appComponent,
+    },
+  ];
+  const data = [
+    {
+      Owner: "Madison Carter",
+      LastCheck: null,
+      OSversion: "Mac OS X 10.14.4",
+      PasswordManager: true,
+      HDencrypted: true,
+      AVinstalled: true,
+      Apllications: 675,
+    },
+    {
+      Owner: "Madison Carter",
+      LastCheck: "2 hours ago",
+      OSversion: "Mac OS X 10.16",
+      PasswordManager: false,
+      HDencrypted: true,
+      AVinstalled: true,
+      Apllications: 275,
+    },
+    {
+      Owner: "Madison Carter",
+      LastCheck: "3 hours ago",
+      OSversion: "Mac OS X 10.16",
+      PasswordManager: false,
+      HDencrypted: true,
+      AVinstalled: true,
+      Apllications: 265,
+    },
+  ];
+  const handleOptions = (target) => {
+    let newOptions = [...options].map((item, index) => {
+      if (index === target) {
+        return { ...item, value: !item.value };
+      } else {
+        return item;
+      }
+    });
+    setOptions(newOptions);
+  };
+  function titleComponent(value) {
+    return <div style={{ fontWeight: "bold" }}>{value}</div>;
+  }
+  function appComponent(value) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        {value}
+      </div>
+    );
+  }
   return (
     <React.Fragment>
-      <DataGrid
-        className={"dx-card wide-card"}
-        dataSource={accessoryData}
-        showBorders={true}
-        focusedRowEnabled={true}
-        columnAutoWidth={true}
-        columnHidingEnabled={false}
-        selectedRowKeys={selectedItemKeys.current}
-        onRowInserting={onRowInserting}
-        onRowUpdating={onRowUpdating}
-        onRowRemoving={onRowRemoving}
-        onExporting={onExporting}
-        onSelectionChanged={selectionChanged}
-        allowColumnReordering={true}
-        allowColumnResizing={true}
-        showColumnLines={false}
-        showRowLines={false}
+      <div
+        className="datagrid-wrapper"
+        style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
       >
-        <LoadPanel enabled={true} />
-        <Editing
-          mode="popup"
-          allowUpdating={true}
-          allowAdding={true}
-          allowDeleting={true}
+        <DataGrid
+          className={"dx-card wide-card"}
+          dataSource={accessoryData}
+          showBorders={true}
+          focusedRowEnabled={true}
+          columnAutoWidth={true}
+          columnHidingEnabled={false}
+          selectedRowKeys={selectedItemKeys.current}
+          onRowInserting={onRowInserting}
+          onRowUpdating={onRowUpdating}
+          onRowRemoving={onRowRemoving}
+          onExporting={onExporting}
+          onSelectionChanged={selectionChanged}
+          allowColumnReordering={true}
+          allowColumnResizing={true}
+          showColumnLines={false}
+          showRowLines={false}
         >
-          <Popup
-            title="Accessory Info"
-            showTitle={true}
-            width={700}
-            height={525}
+          <LoadPanel enabled={true} />
+          <Editing
+            mode="popup"
+            allowUpdating={true}
+            allowAdding={true}
+            allowDeleting={true}
+          >
+            <Popup
+              title="Accessory Info"
+              showTitle={true}
+              width={700}
+              height={525}
+            />
+            <Form>
+              <Item itemType="group" colCount={2} colSpan={2}>
+                <Item dataField="name" />
+                <Item dataField="quantity" />
+                <Item dataField="serialNo" />
+                <Item dataField="purchaseDate" />
+                <Item dataField="purchaseCost" />
+                <Item dataField="orderNo" />
+                <Item dataField="notes" />
+                <Item dataField="imageId" />
+                <Item dataField="categoryId" />
+                <Item dataField="locationId" />
+                <Item dataField="companyId" />
+                <Item dataField="manufacturerId" />
+                <Item dataField="supplierId" />
+                <Item dataField="warranty" />
+                <Item
+                  dataField="note"
+                  itemType="dxTextArea"
+                  colSpan={2}
+                  editorOptions={notesEditorOptions}
+                />
+              </Item>
+            </Form>
+          </Editing>
+          <Paging defaultPageSize={10} />
+          <Pager
+            visible={true}
+            showPageSizeSelector={true}
+            showInfo={false}
+            allowedPageSizes={allowedPageSizes}
           />
-          <Form>
-            <Item itemType="group" colCount={2} colSpan={2}>
-              <Item dataField="name" />
-              <Item dataField="quantity" />
-              <Item dataField="serialNo" />
-              <Item dataField="purchaseDate" />
-              <Item dataField="purchaseCost" />
-              <Item dataField="orderNo" />
-              <Item dataField="notes" />
-              <Item dataField="imageId" />
-              <Item dataField="categoryId" />
-              <Item dataField="locationId" />
-              <Item dataField="companyId" />
-              <Item dataField="manufacturerId" />
-              <Item dataField="supplierId" />
-              <Item dataField="warranty" />
-              <Item
-                dataField="note"
-                itemType="dxTextArea"
-                colSpan={2}
-                editorOptions={notesEditorOptions}
+          <FilterRow visible={false} />
+          <Column dataField="name" caption="Name" dataType="string">
+            <RequiredRule message="Name is required" />
+          </Column>
+          <Column dataField="quantity" caption="Quantity" dataType="number">
+            <RequiredRule message="Quantity is required" />
+            <RangeRule message="Quantity should be minimum 1" min={1} />
+          </Column>
+          <Column dataField="serialNo" caption="Serial No" dataType="string" />
+          <Column
+            dataField="purchaseDate"
+            caption="Purchase Date"
+            dataType="date"
+          />
+          <Column
+            dataField="purchaseCost"
+            caption="Purchase Cost"
+            dataType="number"
+          />
+          <Column dataField="orderNo" caption="Order No" dataType="string" />
+          <Column dataField="cost" caption="Cost" dataType="number" />
+          <Column dataField="imageId" caption="Image">
+            <Lookup dataSource={images} displayExpr="title" valueExpr="id" />
+          </Column>
+          <Column dataField="categoryId" caption="Category">
+            <Lookup dataSource={categories} displayExpr="name" valueExpr="id" />
+          </Column>
+          <Column dataField="conseptId" caption="Consept">
+            <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
+          </Column>
+          <Column dataField="locationId" caption="Location">
+            <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
+          </Column>
+          <Column dataField="companyId" caption="Company">
+            <Lookup dataSource={companies} displayExpr="name" valueExpr="id" />
+          </Column>
+          <Column dataField="manufacturerId" caption="Manufacturer">
+            <Lookup
+              dataSource={manufacturers}
+              valueExpr="id"
+              displayExpr="name"
+            />
+          </Column>
+          <Column dataField="supplierId" caption="Supplier">
+            <Lookup dataSource={suppliers} valueExpr="id" displayExpr="name" />
+          </Column>
+          <Column dataField="warranty" caption="Warranty" dataType="number" />
+          <Column dataField="note" caption="Note" dataType="string" />
+          <Selection mode="multiple" />
+          <Toolbar>
+            <Item location="before">
+              <Button
+                onClick={deleteRecords}
+                icon="trash"
+                text="Delete Items"
               />
             </Item>
-          </Form>
-        </Editing>
-        <Paging defaultPageSize={10} />
-        <Pager
-          visible={true}
-          showPageSizeSelector={true}
-          showInfo={false}
-          allowedPageSizes={allowedPageSizes}
+            <Item name="addRowButton" showText="always" />
+            <Item location="after">
+              <Button icon="refresh" onClick={refreshDataGrid} />
+            </Item>
+            <Item name="exportButton" />
+          </Toolbar>
+          <Export enabled={true} allowExportSelectedData={true} />
+          <ColumnFixing enabled={true} />
+        </DataGrid>
+        <GridTable
+          data={data}
+          columns={columns}
+          hasColumnLines={options[0].value}
+          showPageSize={options[1].value}
+          showPageSizeSelector={options[2].value}
+          showPageSizeInfo={options[3].value}
         />
-        <FilterRow visible={false} />
-        <Column dataField="name" caption="Name" dataType="string">
-          <RequiredRule message="Name is required" />
-        </Column>
-        <Column dataField="quantity" caption="Quantity" dataType="number">
-          <RequiredRule message="Quantity is required" />
-          <RangeRule message="Quantity should be minimum 1" min={1} />
-        </Column>
-        <Column dataField="serialNo" caption="Serial No" dataType="string" />
-        <Column
-          dataField="purchaseDate"
-          caption="Purchase Date"
-          dataType="date"
-        />
-        <Column
-          dataField="purchaseCost"
-          caption="Purchase Cost"
-          dataType="number"
-        />
-        <Column dataField="orderNo" caption="Order No" dataType="string" />
-        <Column dataField="cost" caption="Cost" dataType="number" />
-        <Column dataField="imageId" caption="Image">
-          <Lookup dataSource={images} displayExpr="title" valueExpr="id" />
-        </Column>
-        <Column dataField="categoryId" caption="Category">
-          <Lookup dataSource={categories} displayExpr="name" valueExpr="id" />
-        </Column>
-        <Column dataField="conseptId" caption="Consept">
-          <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
-        </Column>
-        <Column dataField="locationId" caption="Location">
-          <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
-        </Column>
-        <Column dataField="companyId" caption="Company">
-          <Lookup dataSource={companies} displayExpr="name" valueExpr="id" />
-        </Column>
-        <Column dataField="manufacturerId" caption="Manufacturer">
-          <Lookup
-            dataSource={manufacturers}
-            valueExpr="id"
-            displayExpr="name"
-          />
-        </Column>
-        <Column dataField="supplierId" caption="Supplier">
-          <Lookup dataSource={suppliers} valueExpr="id" displayExpr="name" />
-        </Column>
-        <Column dataField="warranty" caption="Warranty" dataType="number" />
-        <Column dataField="note" caption="Note" dataType="string" />
-        <Selection mode="multiple" />
-        <Toolbar>
-          <Item location="before">
-            <Button onClick={deleteRecords} icon="trash" text="Delete Items" />
-          </Item>
-          <Item name="addRowButton" showText="always" />
-          <Item location="after">
-            <Button icon="refresh" onClick={refreshDataGrid} />
-          </Item>
-          <Item name="exportButton" />
-        </Toolbar>
-        <Export enabled={true} allowExportSelectedData={true} />
-        <ColumnFixing enabled={true} />
-      </DataGrid>
+        <div
+          className="button-container"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "1rem",
+          }}
+        >
+          {options.map((item, index) => (
+            <button
+              key={index}
+              onClick={() => handleOptions(index)}
+              style={{
+                width: "220px",
+              }}
+            >
+              {item.option} - {item.value.toString()}
+            </button>
+          ))}
+        </div>
+      </div>
     </React.Fragment>
   );
 };
