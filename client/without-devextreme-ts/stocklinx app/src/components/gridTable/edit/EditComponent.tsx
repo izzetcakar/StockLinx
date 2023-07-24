756acdc473e4b66093da8b4c12981fc068ad0b47
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-24 11:31:35 +0300

    .

--- client/without-devextreme-ts/stocklinx app/src/components/gridTable/edit/EditComponent.tsx
+++ client/without-devextreme-ts/stocklinx app/src/components/gridTable/edit/EditComponent.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./editComponent.scss";
 
 interface EditComponentProps {
-  gridData: any[];
+  datagrid: any[];
   rowIndex: number;
 }
 
-const EditComponent: React.FC<EditComponentProps> = ({ gridData, rowIndex }) => {
+const EditComponent: React.FC<EditComponentProps> = ({ datagrid, rowIndex }) => {
   return (
     <div className="edit-container">
-      <div className="element" onClick={() => console.log(gridData[rowIndex])}>
-        <i className='bx bx-edit-alt' style={{ fontSize: "1.6rem" }}></i>
+      <div className="element" onClick={() => console.log(datagrid[rowIndex])}>
+        <i className='bx bx-edit-alt' ></i>
       </div>
       <div className="element" onClick={() => console.log(rowIndex)}>
-        <i className='bx bxs-x-square' style={{ fontSize: "1.6rem" }}></i>
+        <i className='bx bxs-x-square' ></i>
       </div>
     </div>
   );
