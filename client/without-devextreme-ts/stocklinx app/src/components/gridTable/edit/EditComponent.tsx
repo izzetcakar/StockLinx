4dd720af548aea541958a266ef22906ef12ec48c
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-25 09:19:35 +0300

    Update EditComponent.tsx

--- client/without-devextreme-ts/stocklinx app/src/components/gridTable/edit/EditComponent.tsx
+++ client/without-devextreme-ts/stocklinx app/src/components/gridTable/edit/EditComponent.tsx
@@ -4,16 +4,41 @@ import "./editComponent.scss";
 interface EditComponentProps {
   datagrid: any[];
   rowIndex: number;
+  onRowUpdate: (row: object) => void;
+  onRowDelete: (row: object) => void;
+  onStartEdit?: (row: object) => void;
 }
 
-const EditComponent: React.FC<EditComponentProps> = ({ datagrid, rowIndex }) => {
+const EditComponent: React.FC<EditComponentProps> = ({
+  datagrid,
+  rowIndex,
+  onRowUpdate,
+  onRowDelete,
+  onStartEdit,
+}) => {
+
+  const handleEditClick = () => {
+    const row = datagrid[rowIndex];
+    if (onStartEdit) {
+      onStartEdit(row);
+    }
+    onRowUpdate(row);
+  };
+
+  const handleDeleteClick = () => {
+    const row = datagrid[rowIndex];
+    if (onRowDelete) {
+      onRowDelete(row);
+    }
+  };
+
   return (
     <div className="edit-container">
-      <div className="element" onClick={() => console.log(datagrid[rowIndex])}>
-        <i className='bx bx-edit-alt' ></i>
+      <div className="element" onClick={handleEditClick}>
+        <i className="bx bx-edit-alt"></i>
       </div>
-      <div className="element" onClick={() => console.log(rowIndex)}>
-        <i className='bx bxs-x-square' ></i>
+      <div className="element" onClick={handleDeleteClick}>
+        <i className="bx bxs-x-square"></i>
       </div>
     </div>
   );
