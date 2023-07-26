08a66f4a5da497e8c86f141a38c7678cb76361b1
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-26 15:51:43 +0300

    Update departmentReducer.ts

--- client/stocklinx app/src/redux/departmentReducer.ts
+++ client/stocklinx app/src/redux/departmentReducer.ts
@@ -1,80 +1,89 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { request } from "../server/api";
+import { generateId } from "../functions/generateId";
+import { IDepartment } from "../interfaces/interfaces";
 const requestUrl = "Department/";
-interface Department {}
 
-export const getAllDepartments = createAsyncThunk<Department[]>(
+const baseDepartment: IDepartment = {
+  id: generateId(),
+  name: "",
+  createdDate: null,
+  updatedDate: null,
+  deletedDate: null,
+  imagePath: null,
+  companyId: null,
+  locationId: null,
+  managerId: null,
+  notes: null,
+};
+
+export const getAllDepartments = createAsyncThunk(
   "departments/getAll",
-  async (): Promise<Department[]> => {
-    const response = await request<Department[]>({
-      requestUrl: "Department/",
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IDepartment>({
+      requestUrl: requestUrl,
       apiType: "get",
     });
-    return response.data as Department[];
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const getDepartmentById = createAsyncThunk(
   "departments/getById",
-  async (Id): Promise<Department> => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IDepartment>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "get",
     });
-    return response.data as Department;
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const createDepartment = createAsyncThunk(
   "departments/create",
-  async (department, { fulfillWithValue, rejectWithValue }) => {
-    const response = await request({
+  async (department: IDepartment, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IDepartment>({
       requestUrl: requestUrl,
       queryData: department,
       apiType: "post",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const updateDepartment = createAsyncThunk(
   "departments/update",
-  async (department, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (department: IDepartment, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IDepartment>({
       requestUrl: requestUrl,
       queryData: department,
       apiType: "put",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const removeDepartment = createAsyncThunk(
   "departments/remove",
-  async (Id, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IDepartment>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "delete",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 interface State {
-  items: Department[];
+  department: IDepartment;
+  departments: IDepartment[];
   status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: State = {
-  items: [],
+  department: baseDepartment,
+  departments: [],
   status: "idle",
   error: null,
 };
@@ -83,22 +92,59 @@ const departmentSlice = createSlice({
   name: "department",
   initialState,
   reducers: {
-    clearDepartments: (state) => {
-      state.items = [];
+    setDepartment: (state, action: PayloadAction<IDepartment>) => {
+      state.department = action.payload;
     },
-    setDepartments: (state, action) => {
-      state.items = action.payload;
+    setDepartments: (state, action: PayloadAction<IDepartment[]>) => {
+      state.departments = action.payload;
+    },
+    clearDepartment: (state) => {
+      state.department = baseDepartment;
+    },
+    clearDepartments: (state) => {
+      state.departments = [];
     },
   },
   extraReducers(builder) {
-    builder.addCase(
-      getAllDepartments.fulfilled,
-      (state, action: PayloadAction<Department[]>) => {
-        state.items = action.payload;
-      }
-    );
+    builder.addCase(getAllDepartments.fulfilled, (state, action) => {
+      state.error = null;
+      state.departments = action.payload as IDepartment[];
+    });
+    builder.addCase(getAllDepartments.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(getDepartmentById.fulfilled, (state, action) => {
+      state.department = action.payload as IDepartment;
+      state.error = null;
+    });
+    builder.addCase(getDepartmentById.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createDepartment.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createDepartment.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(updateDepartment.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(updateDepartment.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(removeDepartment.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(removeDepartment.fulfilled, (state) => {
+      state.error = null;
+    });
   },
 });
 
-export const { setDepartments, clearDepartments } = departmentSlice.actions;
+export const {
+  setDepartment,
+  setDepartments,
+  clearDepartment,
+  clearDepartments,
+} = departmentSlice.actions;
 export default departmentSlice.reducer;
