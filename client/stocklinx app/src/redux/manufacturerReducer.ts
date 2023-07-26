c5387e100afc090cb534136305065e69676b608c
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-26 15:51:21 +0300

    Update manufacturerReducer.ts

--- client/stocklinx app/src/redux/manufacturerReducer.ts
+++ client/stocklinx app/src/redux/manufacturerReducer.ts
@@ -1,80 +1,94 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { request } from "../server/api";
+import { generateId } from "../functions/generateId";
+import { IManufacturer } from "../interfaces/interfaces";
 const requestUrl = "Manufacturer/";
-interface Manufacturer {}
 
-export const getAllManufacturers = createAsyncThunk<Manufacturer[]>(
+const baseManufacturer: IManufacturer = {
+  id: generateId(),
+  name: "",
+  createdDate: null,
+  updatedDate: null,
+  deletedDate: null,
+  imagePath: null,
+  supportEmail: null,
+  supportPhone: null,
+  website: null,
+};
+
+export const getAllManufacturers = createAsyncThunk(
   "manufacturers/getAll",
-  async (): Promise<Manufacturer[]> => {
-    const response = await request<Manufacturer[]>({
-      requestUrl: "Manufacturer/",
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IManufacturer>({
+      requestUrl: requestUrl,
       apiType: "get",
     });
-    return response.data as Manufacturer[];
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const getManufacturerById = createAsyncThunk(
   "manufacturers/getById",
-  async (Id): Promise<Manufacturer> => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IManufacturer>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "get",
     });
-    return response.data as Manufacturer;
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const createManufacturer = createAsyncThunk(
   "manufacturers/create",
-  async (manufacturer, { fulfillWithValue, rejectWithValue }) => {
-    const response = await request({
+  async (
+    manufacturer: IManufacturer,
+    { fulfillWithValue, rejectWithValue }
+  ) => {
+    const response = await request<IManufacturer>({
       requestUrl: requestUrl,
       queryData: manufacturer,
       apiType: "post",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const updateManufacturer = createAsyncThunk(
   "manufacturers/update",
-  async (manufacturer, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (
+    manufacturer: IManufacturer,
+    { fulfillWithValue, rejectWithValue }
+  ) => {
+    const response = await request<IManufacturer>({
       requestUrl: requestUrl,
       queryData: manufacturer,
       apiType: "put",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const removeManufacturer = createAsyncThunk(
   "manufacturers/remove",
-  async (Id, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<IManufacturer>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "delete",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 interface State {
-  items: Manufacturer[];
+  manufacturer: IManufacturer;
+  manufacturers: IManufacturer[];
   status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: State = {
-  items: [],
+  manufacturer: baseManufacturer,
+  manufacturers: [],
   status: "idle",
   error: null,
 };
@@ -83,23 +97,59 @@ const manufacturerSlice = createSlice({
   name: "manufacturer",
   initialState,
   reducers: {
-    clearManufacturers: (state) => {
-      state.items = [];
+    setManufacturer: (state, action: PayloadAction<IManufacturer>) => {
+      state.manufacturer = action.payload;
     },
-    setManufacturers: (state, action) => {
-      state.items = action.payload;
+    setManufacturers: (state, action: PayloadAction<IManufacturer[]>) => {
+      state.manufacturers = action.payload;
+    },
+    clearManufacturer: (state) => {
+      state.manufacturer = baseManufacturer;
+    },
+    clearManufacturers: (state) => {
+      state.manufacturers = [];
     },
   },
   extraReducers(builder) {
-    builder.addCase(
-      getAllManufacturers.fulfilled,
-      (state, action: PayloadAction<Manufacturer[]>) => {
-        state.items = action.payload;
-      }
-    );
+    builder.addCase(getAllManufacturers.fulfilled, (state, action) => {
+      state.error = null;
+      state.manufacturers = action.payload as IManufacturer[];
+    });
+    builder.addCase(getAllManufacturers.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(getManufacturerById.fulfilled, (state, action) => {
+      state.manufacturer = action.payload as IManufacturer;
+      state.error = null;
+    });
+    builder.addCase(getManufacturerById.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createManufacturer.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createManufacturer.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(updateManufacturer.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(updateManufacturer.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(removeManufacturer.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(removeManufacturer.fulfilled, (state) => {
+      state.error = null;
+    });
   },
 });
 
-export const { setManufacturers, clearManufacturers } =
-  manufacturerSlice.actions;
+export const {
+  setManufacturer,
+  setManufacturers,
+  clearManufacturer,
+  clearManufacturers,
+} = manufacturerSlice.actions;
 export default manufacturerSlice.reducer;
