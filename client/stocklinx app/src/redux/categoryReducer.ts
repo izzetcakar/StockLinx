d7548f5a7706ae343c1bd33ca7ca25d3184a712f
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-26 15:51:50 +0300

    Update categoryReducer.ts

--- client/stocklinx app/src/redux/categoryReducer.ts
+++ client/stocklinx app/src/redux/categoryReducer.ts
@@ -1,80 +1,85 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { request } from "../server/api";
+import { generateId } from "../functions/generateId";
+import { ICategory } from "../interfaces/interfaces";
 const requestUrl = "Category/";
-interface Category {}
 
-export const getAllCategories = createAsyncThunk<Category[]>(
+const baseCategory: ICategory = {
+  id: generateId(),
+  name: "",
+  createdDate: null,
+  updatedDate: null,
+  deletedDate: null,
+  imagePath: null,
+};
+
+export const getAllCategories = createAsyncThunk(
   "categories/getAll",
-  async (): Promise<Category[]> => {
-    const response = await request<Category[]>({
-      requestUrl: "Category/",
+  async (_, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<ICategory>({
+      requestUrl: requestUrl,
       apiType: "get",
     });
-    return response.data as Category[];
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const getCategoryById = createAsyncThunk(
   "categories/getById",
-  async (Id): Promise<Category> => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<ICategory>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "get",
     });
-    return response.data as Category;
+    if (!response.success) return fulfillWithValue(response.data);
+    return rejectWithValue(response.message);
   }
 );
 export const createCategory = createAsyncThunk(
   "categories/create",
-  async (category, { fulfillWithValue, rejectWithValue }) => {
-    const response = await request({
+  async (category: ICategory, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<ICategory>({
       requestUrl: requestUrl,
       queryData: category,
       apiType: "post",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const updateCategory = createAsyncThunk(
   "categories/update",
-  async (category, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (category: ICategory, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<ICategory>({
       requestUrl: requestUrl,
       queryData: category,
       apiType: "put",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 export const removeCategory = createAsyncThunk(
   "categories/remove",
-  async (Id, { rejectWithValue, fulfillWithValue }) => {
-    const response = await request({
+  async (Id: string, { fulfillWithValue, rejectWithValue }) => {
+    const response = await request<ICategory>({
       requestUrl: requestUrl + `${Id}`,
       apiType: "delete",
     });
-    if (!response.success) {
-      return rejectWithValue(response.message);
-    } else {
-      return fulfillWithValue(null);
-    }
+    if (!response.success) return fulfillWithValue(null);
+    return rejectWithValue(response.message);
   }
 );
 interface State {
-  items: Category[];
+  category: ICategory;
+  categories: ICategory[];
   status: "idle" | "loading" | "succeeded" | "failed";
   error: string | null;
 }
 
 const initialState: State = {
-  items: [],
+  category: baseCategory,
+  categories: [],
   status: "idle",
   error: null,
 };
@@ -83,22 +88,55 @@ const categorySlice = createSlice({
   name: "category",
   initialState,
   reducers: {
-    clearCategories: (state) => {
-      state.items = [];
+    setCategory: (state, action: PayloadAction<ICategory>) => {
+      state.category = action.payload;
     },
-    setCategories: (state, action) => {
-      state.items = action.payload;
+    setCategories: (state, action: PayloadAction<ICategory[]>) => {
+      state.categories = action.payload;
+    },
+    clearCategory: (state) => {
+      state.category = baseCategory;
+    },
+    clearCategories: (state) => {
+      state.categories = [];
     },
   },
   extraReducers(builder) {
-    builder.addCase(
-      getAllCategories.fulfilled,
-      (state, action: PayloadAction<Category[]>) => {
-        state.items = action.payload;
-      }
-    );
+    builder.addCase(getAllCategories.fulfilled, (state, action) => {
+      state.error = null;
+      state.categories = action.payload as ICategory[];
+    });
+    builder.addCase(getAllCategories.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(getCategoryById.fulfilled, (state, action) => {
+      state.category = action.payload as ICategory;
+      state.error = null;
+    });
+    builder.addCase(getCategoryById.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createCategory.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(createCategory.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(updateCategory.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(updateCategory.fulfilled, (state) => {
+      state.error = null;
+    });
+    builder.addCase(removeCategory.rejected, (state, action) => {
+      state.error = action.payload as string;
+    });
+    builder.addCase(removeCategory.fulfilled, (state) => {
+      state.error = null;
+    });
   },
 });
 
-export const { setCategories, clearCategories } = categorySlice.actions;
+export const { setCategory, setCategories, clearCategory, clearCategories } =
+  categorySlice.actions;
 export default categorySlice.reducer;
