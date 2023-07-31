91b7aaa0a1dc43a54c6fe9ec25555dba70a5e4eb
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-31 12:03:48 +0300

    Create ProductStatusController.cs

---
+++ server/StockLinx.API/Controllers/ProductStatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockLinx.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductStatusController : CustomBaseController
+    {
+
+    }
+}
