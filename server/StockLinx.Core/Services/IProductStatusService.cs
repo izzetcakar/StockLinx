f7152b4221e211d622b208c01b1d611b7004f9ef
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-31 12:03:53 +0300

    Create IProductStatusService.cs

---
+++ server/StockLinx.Core/Services/IProductStatusService.cs
@@ -0,0 +1,13 @@
+﻿using StockLinx.Core.DTOs.Create;
+using StockLinx.Core.DTOs.Update;
+using StockLinx.Core.Entities;
+
+namespace StockLinx.Core.Services
+{
+    public interface IProductStatusService : IService<ProductStatus>
+    {
+        Task CreateProductStatusAsync(ProductStatusCreateDto createDto);
+        Task UpdateProductStatusAsync(ProductStatusUpdateDto updateDto);
+        Task DeleteProductStatusAsync(Guid productStatusId);
+    }
+}
