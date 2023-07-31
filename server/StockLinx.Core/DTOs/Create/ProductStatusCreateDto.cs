3822a5d4b3d2d7b8729f75720299c5200ce0752c
İzzet Çakar <izzetcakar4@gmail.com>
2023-07-31 12:03:38 +0300

    Update ProductStatusDtos

---
+++ server/StockLinx.Core/DTOs/Create/ProductStatusCreateDto.cs
@@ -0,0 +1,9 @@
+﻿using StockLinx.Core.DTOs.Generic;
+
+namespace StockLinx.Core.DTOs.Create
+{
+    public class ProductStatusCreateDto : BaseDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ server/StockLinx.Core/DTOs/Update/ProductStatusUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using StockLinx.Core.DTOs.Generic;
+
+namespace StockLinx.Core.DTOs.Update
+{
+    public class ProductStatusUpdateDto : BaseDto
+    {
+        public string Name { get; set; }
+    }
+}
