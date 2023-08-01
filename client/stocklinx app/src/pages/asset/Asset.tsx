e8395b1609c92843598132f69b6d00aa7b1f9294
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-01 11:53:39 +0300

    Update Asset.tsx

--- client/stocklinx app/src/pages/asset/Asset.tsx
+++ client/stocklinx app/src/pages/asset/Asset.tsx
@@ -5,82 +5,74 @@ import AssetForm from "../../components/form/product/asset/AssetForm";
 import GridTable from "../../components/gridTable/GridTable";
 import CustomPopup from "../../components/popup/CustomPopup";
 import { IAsset } from "../../interfaces/interfaces";
-import { useAppDispatch } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import { clearAsset, setAsset } from "../../redux/assetReducer";
+import { RootState } from "../../redux/store";
 
 const Asset = () => {
   const dispatch = useAppDispatch();
   const [formVisible, setFormVisible] = useState<boolean>(false);
-  const [showColumnLines, setShowColumnLines] = useState<boolean>(false);
+  const assets = useAppSelector((state: RootState) => state.asset.assets);
+  const categories = useAppSelector((state: RootState) => state.category.categories);
 
-  const TitleComponent: React.FC<{ value: string }> = ({ value }) => {
-    return <div style={{ fontWeight: "bold" }}>{value}</div>;
-  };
-  const AppComponent: React.FC<{ value: number }> = ({ value }) => {
+
+  const getCategoryById: React.FC<{ value: string }> = ({ value }) => {
+    const category = categories.find((category) => category.id === value);
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
-        {value}
+      <div>
+        {category?.name}
       </div>
     );
   };
   const columns = [
     {
-      dataField: "Owner",
-      caption: "Owner",
+      dataField: "categoryId",
+      caption: "Category",
       dataType: "string",
-      renderComponent: TitleComponent,
+      renderComponent: getCategoryById,
     },
-    { dataField: "LastCheck", caption: "Last Check", dataType: "string" },
-    { dataField: "OSversion", caption: "OS Version", dataType: "string" },
     {
-      dataField: "PasswordManager",
-      caption: "Password Manager",
-      dataType: "boolean",
+      dataField: "locationId",
+      caption: "Location",
+      dataType: "string",
     },
-    { dataField: "HDencrypted", caption: "HD Encrypted", dataType: "boolean" },
-    { dataField: "AVinstalled", caption: "AV Installed", dataType: "boolean" },
     {
-      dataField: "Apllications",
-      caption: "Apllications",
+      dataField: "companyId",
+      caption: "Company",
+      dataType: "string",
+    },
+    {
+      dataField: "manufacturerId",
+      caption: "Manufacturer",
+      dataType: "string",
+    },
+    {
+      dataField: "modelId",
+      caption: "Model",
+      dataType: "string",
+    },
+    {
+      dataField: "statusId",
+      caption: "Status",
+      dataType: "string",
+    },
+    {
+      dataField: "name",
+      caption: "Name",
+      dataType: "string",
+    },
+    { dataField: "serialNo", caption: "Serial No", dataType: "string" },
+    { dataField: "orderNo", caption: "Order No", dataType: "string" },
+    { dataField: "tagNo", caption: "Tag No", dataType: "string" },
+    {
+      dataField: "purchaseCost",
+      caption: "Purchase Cost",
       dataType: "number",
-      renderComponent: AppComponent,
     },
+    { dataField: "purchaseDate", caption: "Purchase Date", dataType: "date" },
+    { dataField: "notes", caption: "Notes", dataType: "string" },
   ];
-  const data = [
-    {
-      Owner: "Madison Carter",
-      LastCheck: null,
-      OSversion: "Mac OS X 10.14.4",
-      PasswordManager: true,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 675,
-    },
-    {
-      Owner: "Madison Carter",
-      LastCheck: "2 hours ago",
-      OSversion: "Mac OS X 10.16",
-      PasswordManager: false,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 275,
-    },
-    {
-      Owner: "Madison Carter",
-      LastCheck: "3 hours ago",
-      OSversion: "Mac OS X 10.16",
-      PasswordManager: false,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 265,
-    },
-  ];
+
   const handleFormVisible = () => {
     setFormVisible((prevFormVisible) => !prevFormVisible);
   };
@@ -94,29 +86,7 @@ const Asset = () => {
   };
   const onRowUpdate = (row: object) => {
     console.log(row);
-    openAssetModal({
-      id: "asdas",
-      manufacturerId: null,
-      modelId: null,
-      notes: "notes",
-      createdDate: null,
-      deletedDate: null,
-      updatedDate: null,
-      categoryId: null,
-      locationId: null,
-      companyId: "",
-      statusId: null,
-      imagePath: "",
-      name: "Update Asset",
-      orderNo: "",
-      purchaseDate: null,
-      purchaseCost: 100,
-      checkInCounter: null,
-      checkOutCounter: null,
-      serialNo: "",
-      tagNo: "",
-      overageAssets: null,
-    });
+    openAssetModal(row as IAsset);
   };
   const onRowDelete = (row: object) => {
     console.log("delete", row);
@@ -126,7 +96,7 @@ const Asset = () => {
   };
 
   const openAssetModal = (asset?: IAsset) => modals.open({
-    modalId: 'edit-modal',
+    modalId: 'asset-modal',
     title: 'Update',
     children: (
       <AssetForm asset={asset} submitFunc={handleUpdate} />
@@ -139,9 +109,9 @@ const Asset = () => {
       style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
     >
       <GridTable
-        data={data}
+        data={assets}
         columns={columns}
-        hasColumnLines={showColumnLines}
+        hasColumnLines={false}
         cellCssClass="testClass"
         pageSizes={[1, 2, 5]}
         enableEdit={true}
@@ -151,23 +121,6 @@ const Asset = () => {
         onRowDelete={onRowDelete}
         onStartEdit={onStartEdit}
       />
-      <div
-        className="button-container"
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "1rem",
-        }}
-      >
-        <button
-          onClick={() => setShowColumnLines(prev => !prev)}
-          style={{
-            width: "220px",
-          }}
-        >
-          hasColumnLines: {showColumnLines ? "true" : "false"}
-        </button>
-      </div>
       <CustomPopup
         visible={formVisible}
         title="Custom Form"
