9241ac69f18b9fb6c565c01571405f32dad96725
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-01 11:52:40 +0300

    Create ProductStatusForm.tsx

---
+++ client/stocklinx app/src/components/form/productStatus/ProductStatusForm.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { TextInput, Button, Group, FileInput, rem, Image, ScrollArea, Flex, Textarea } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { closeModal } from '@mantine/modals';
+import { IconUpload } from '@tabler/icons-react';
+import { modals } from '@mantine/modals';
+import { v4 as uuidv4 } from "uuid";
+import { IProductStatus } from '../../../interfaces/interfaces';
+import { handleImageChange } from '../functions/formFunctions';
+
+interface ProductStatusFormProps {
+    productStatus?: IProductStatus;
+    submitFunc: (data: object) => void;
+}
+
+const ProductStatusForm: React.FC<ProductStatusFormProps> = ({
+    productStatus,
+    submitFunc = () => console.log("submit"),
+}) => {
+
+    const form = useForm<IProductStatus>({
+        initialValues: productStatus ? { ...productStatus } : {
+            id: uuidv4(),
+            name: "",
+        },
+        validate: {
+            name: (value: string) => (/(?!^$)([^\s])/.test(value) ? null : 'Name should not be empty'),
+        },
+    });
+    const handleSubmit = (data: object) => {
+        console.log(data);
+        submitFunc(data);
+        closeModal("product-status-modal");
+    };
+    const openNextModel = () => modals.open({
+        modalId: 'next-modal',
+        title: 'Page 2',
+        children: (
+            <Button fullWidth onClick={() => closeModal("next-modal")} color='dark'>Back</Button>
+        ),
+    });
+
+    return (
+        <form onSubmit={form.onSubmit((values) => handleSubmit(values))} >
+            <ScrollArea type="auto">
+                <Flex direction="column" gap={10} mx="auto" maw="auto" px={40}>
+                    <TextInput
+                        label="Name"
+                        placeholder="New Name"
+                        {...form.getInputProps("name")}
+                    />
+                    <Group position="right" mt="md">
+                        <Button type="submit" color="dark">
+                            Submit
+                        </Button>
+                        <Button onClick={() => openNextModel()} color="dark">
+                            Next Modal
+                        </Button>
+                    </Group>
+                </Flex>
+            </ScrollArea>
+        </form >
+    );
+}
+
+export default ProductStatusForm
