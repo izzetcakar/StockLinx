7178e8ffa96a5204a90604128d4c57654d529706
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-01 11:53:42 +0300

    Update Accessory.tsx

--- client/stocklinx app/src/pages/accessory/Accessory.tsx
+++ client/stocklinx app/src/pages/accessory/Accessory.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./accessory.scss";
 import { modals } from '@mantine/modals';
 import TestForm from "../../components/form/TestForm";
@@ -6,80 +6,78 @@ import AccessoryForm from "../../components/form/product/accessory/AccessoryForm
 import GridTable from "../../components/gridTable/GridTable";
 import CustomPopup from "../../components/popup/CustomPopup";
 import { IAccessory } from "../../interfaces/interfaces";
-import { useAppDispatch } from "../../hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks";
 import { clearAccessory, setAccessory } from "../../redux/accessoryReducer";
+import { RootState } from "../../redux/store";
 
 const Accessory = () => {
   const dispatch = useAppDispatch();
+  const accessories = useAppSelector((state: RootState) => state.accessory.accessories);
+  const categories = useAppSelector((state: RootState) => state.category.categories);
+  // const locations = useAppSelector((state: RootState) => state.location.locations);
+  // const companies = useAppSelector((state: RootState) => state.company.companies);
+  // const productStatuses = useAppSelector((state: RootState) => state.productStatus.productStatuses);
+  // const manufacturers = useAppSelector((state: RootState) => state.manufacturer.manufacturers);
+  // const models = useAppSelector((state: RootState) => state.model.models);
+
   const [formVisible, setFormVisible] = useState<boolean>(false);
-  const [showColumnLines, setShowColumnLines] = useState<boolean>(false);
-  const TitleComponent: React.FC<{ value: string }> = ({ value }) => {
-    return <div style={{ fontWeight: "bold" }}>{value}</div>;
-  };
-  const AppComponent: React.FC<{ value: number }> = ({ value }) => {
+
+  const getCategoryById: React.FC<{ value: string }> = ({ value }) => {
+    const category = categories.find((category) => category.id === value);
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100%",
-        }}
-      >
-        {value}
+      <div>
+        {category?.name}
       </div>
     );
   };
   const columns = [
     {
-      dataField: "Owner",
-      caption: "Owner",
+      dataField: "categoryId",
+      caption: "Category",
       dataType: "string",
-      renderComponent: TitleComponent,
+      renderComponent: getCategoryById,
     },
-    { dataField: "LastCheck", caption: "Last Check", dataType: "string" },
-    { dataField: "OSversion", caption: "OS Version", dataType: "string" },
     {
-      dataField: "PasswordManager",
-      caption: "Password Manager",
-      dataType: "boolean",
+      dataField: "locationId",
+      caption: "Location",
+      dataType: "string",
     },
-    { dataField: "HDencrypted", caption: "HD Encrypted", dataType: "boolean" },
-    { dataField: "AVinstalled", caption: "AV Installed", dataType: "boolean" },
     {
-      dataField: "Apllications",
-      caption: "Apllications",
+      dataField: "companyId",
+      caption: "Company",
+      dataType: "string",
+    },
+    {
+      dataField: "manufacturerId",
+      caption: "Manufacturer",
+      dataType: "string",
+    },
+    {
+      dataField: "supplierId",
+      caption: "Supplier",
+      dataType: "string",
+    },
+    {
+      dataField: "statusId",
+      caption: "Status",
+      dataType: "string",
+    },
+    {
+      dataField: "name",
+      caption: "Name",
+      dataType: "string",
+    },
+    { dataField: "serialNo", caption: "Serial No", dataType: "string" },
+    { dataField: "orderNo", caption: "Order No", dataType: "string" },
+    { dataField: "quantity", caption: "Quantity", dataType: "number" },
+    {
+      dataField: "purchaseCost",
+      caption: "Purchase Cost",
       dataType: "number",
-      renderComponent: AppComponent,
     },
-  ];
-  const data = [
-    {
-      Owner: "Madison Carter",
-      LastCheck: null,
-      OSversion: "Mac OS X 10.14.4",
-      PasswordManager: true,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 675,
-    },
-    {
-      Owner: "Madison Carter",
-      LastCheck: "2 hours ago",
-      OSversion: "Mac OS X 10.16",
-      PasswordManager: false,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 275,
-    },
-    {
-      Owner: "Madison Carter",
-      LastCheck: "3 hours ago",
-      OSversion: "Mac OS X 10.16",
-      PasswordManager: false,
-      HDencrypted: true,
-      AVinstalled: true,
-      Apllications: 265,
-    },
+    { dataField: "purchaseDate", caption: "Purchase Date", dataType: "date" },
+    { dataField: "warrantyDate", caption: "Warranty Date", dataType: "date" },
+    { dataField: "notes", caption: "Notes", dataType: "string" },
   ];
   const handleFormVisible = () => {
     setFormVisible((prevFormVisible) => !prevFormVisible);
@@ -94,24 +92,7 @@ const Accessory = () => {
   };
   const onRowUpdate = (row: object) => {
     console.log(row);
-    openAccessoryModal({
-      id: "asdas",
-      manufacturerId: null,
-      supplierId: null,
-      categoryId: "",
-      locationId: "",
-      companyId: "",
-      statusId: "",
-      name: "Update Accessory",
-      imagePath: "",
-      serialNo: "",
-      orderNo: "",
-      purchaseCost: 100,
-      purchaseDate: null,
-      warrantyDate: null,
-      quantity: 1,
-      notes: "",
-    });
+    openAccessoryModal(row as IAccessory);
   };
   const onRowDelete = (row: object) => {
     console.log("delete", row);
@@ -134,10 +115,9 @@ const Accessory = () => {
       style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
     >
       <GridTable
-        data={data}
+        data={accessories}
         columns={columns}
-        hasColumnLines={showColumnLines}
-        cellCssClass="testClass"
+        hasColumnLines={false}
         pageSizes={[1, 2, 5]}
         enableEdit={true}
         showPageSize={true}
@@ -146,23 +126,6 @@ const Accessory = () => {
         onRowDelete={onRowDelete}
         onStartEdit={onStartEdit}
       />
-      <div
-        className="button-container"
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "1rem",
-        }}
-      >
-        <button
-          onClick={() => setShowColumnLines(prev => !prev)}
-          style={{
-            width: "220px",
-          }}
-        >
-          hasColumnLines: {showColumnLines ? "true" : "false"}
-        </button>
-      </div>
       <CustomPopup
         visible={formVisible}
         title="Custom Form"
