8242a5a0ca8da7c3cfec044cd73140e97c1abceb
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-01 11:53:31 +0300

    Update Consumable.tsx

--- client/stocklinx app/src/pages/consumable/Consumable.tsx
+++ client/stocklinx app/src/pages/consumable/Consumable.tsx
@@ -1,241 +1,147 @@
-// import React, { useEffect, useRef } from "react";
-// import "devextreme/data/odata/store";
-// import { Workbook } from "exceljs";
-// import { saveAs } from "file-saver-es";
-// import { exportDataGrid } from "devextreme/excel_exporter";
-// import DataGrid, {
-//   Column,
-//   Editing,
-//   Popup,
-//   Paging,
-//   Lookup,
-//   Form,
-//   Pager,
-//   FilterRow,
-//   Export,
-//   Selection,
-//   Toolbar,
-//   ColumnFixing,
-//   LoadPanel,
-// } from "devextreme-react/data-grid";
-// import { Item, RequiredRule, RangeRule } from "devextreme-react/form";
-// import { useDispatch, useSelector } from "react-redux";
-// import DataSource from "devextreme/data/data_source";
-// import { Button } from "devextreme-react";
-// import { notifyError } from "../../functions/notifyError";
-// import { getAllProducts } from "../../redux/productReducer";
-
-// const Consumable = ({ Id }) => {
-//   const dispatch = useDispatch();
-//   const effect = useRef(false);
-//   const selectedItemKeys = useRef([]);
-//   const consumables = useSelector((state) => state.product.consumables);
-//   const images = useSelector((state) => state.image.items);
-//   const categories = useSelector((state) => state.category.items);
-//   const locations = useSelector((state) => state.location.items);
-//   const companies = useSelector((state) => state.company.items);
-
-//   const notesEditorOptions = { height: "auto" };
-//   const allowedPageSizes = [5, 10, 20, 50, 100];
-
-//   useEffect(() => {
-//     if (effect.current === false) {
-//       return () => {
-//         effect.current = true;
-//       };
-//     } else {
-//       refreshDataGrid();
-//     }
-//   }, [Id]);
-
-//   const refreshDataGrid = () => {
-//     // dispatch(getConsumables());
-//   };
-
-//   const consumableData = new DataSource({
-//     store: {
-//       type: "array",
-//       data: JSON.parse(JSON.stringify(consumables)),
-//       // key: "id",
-//     },
-//   });
-//   const onExporting = (e) => {
-//     const workbook = new Workbook();
-//     const worksheet = workbook.addWorksheet("Main sheet");
-
-//     exportDataGrid({
-//       component: e.component,
-//       worksheet,
-//       autoFilterEnabled: true,
-//     }).then(() => {
-//       workbook.xlsx.writeBuffer().then((buffer) => {
-//         saveAs(
-//           new Blob([buffer], { type: "application/octet-stream" }),
-//           "Consumables.xlsx"
-//         );
-//       });
-//     });
-//     e.cancel = true;
-//   };
-//   const onRowUpdating = async (e) => {
-//     // const res = await dispatch(updateConsumable({ ...e.oldData, ...e.newData }));
-//     // notifyError(res);
-//     // dispatch(getConsumables());
-//   };
-//   const onRowInserting = async (e) => {
-//     // const res = await dispatch(createConsumable({ ...e.data, checklistId: Id }));
-//     // notifyError(res);
-//     // dispatch(getConsumables());
-//   };
-//   const onRowRemoving = async (e) => {
-//     // const res = await dispatch(removeConsumable(e.data.id));
-//     // notifyError(res);
-//     // dispatch(getConsumables());
-//   };
-//   const deleteRecords = async () => {
-//     let inputIds = [];
-//     selectedItemKeys.current.forEach((key) => {
-//       inputIds.push(key);
-//     });
-//     // const res = await dispatch(deleteSelectedAcc(inputIds));
-//     // notifyError(res);
-//     // selectedItemKeys.current = [];
-//     // dispatch(getConsumables());
-//   };
-//   const selectionChanged = (data) => {
-//     selectedItemKeys.current = data.selectedRowKeys;
-//   };
-
-//   return (
-//     <React.Fragment>
-//       <DataGrid
-//         className={"dx-card wide-card"}
-//         dataSource={consumableData}
-//         height={"100%"}
-//         showBorders={true}
-//         focusedRowEnabled={true}
-//         columnAutoWidth={true}
-//         columnHidingEnabled={false}
-//         selectedRowKeys={selectedItemKeys.current}
-//         onRowInserting={onRowInserting}
-//         onRowUpdating={onRowUpdating}
-//         onRowRemoving={onRowRemoving}
-//         onExporting={onExporting}
-//         onSelectionChanged={selectionChanged}
-//         allowColumnReordering={true}
-//         allowColumnResizing={true}
-//       >
-//         <LoadPanel enabled={true} />
-//         <Editing
-//           mode="popup"
-//           allowUpdating={true}
-//           allowAdding={true}
-//           allowDeleting={true}
-//         >
-//           <Popup
-//             title="List Item Info"
-//             showTitle={true}
-//             width={700}
-//             height={525}
-//           />
-//           <Form>
-//             <Item itemType="group" colCount={2} colSpan={2}>
-//               <Item dataField="name" />
-//               <Item dataField="quantity" />
-//               <Item dataField="serialNo" />
-//               <Item dataField="purchaseDate" />
-//               <Item dataField="purchaseCost" />
-//               <Item dataField="orderNo" />
-//               <Item dataField="notes" />
-//               <Item dataField="imageId" />
-//               <Item dataField="categoryId" />
-//               <Item dataField="locationId" />
-//               <Item dataField="companyId" />
-//               <Item dataField="modelNo" />
-//               <Item dataField="itemNo" />
-//               <Item dataField="tag" />
-//               <Item
-//                 dataField="note"
-//                 itemType="dxTextArea"
-//                 colSpan={2}
-//                 editorOptions={notesEditorOptions}
-//               />
-//             </Item>
-//           </Form>
-//         </Editing>
-//         <Paging defaultPageSize={10} />
-//         <Pager
-//           visible={true}
-//           showPageSizeSelector={true}
-//           showInfo={true}
-//           allowedPageSizes={allowedPageSizes}
-//         />
-//         <FilterRow visible={false} />
-//         <Column dataField="name" caption="Name" dataType="string">
-//           <RequiredRule message="Name is required" />
-//         </Column>
-//         <Column dataField="quantity" caption="Quantity" dataType="number">
-//           <RequiredRule message="Quantity is required" />
-//           <RangeRule message="Quantity should be minimum 1" min={1} />
-//         </Column>
-//         <Column dataField="serialNo" caption="Serial No" dataType="string" />
-//         <Column
-//           dataField="purchaseDate"
-//           caption="Purchase Date"
-//           dataType="date"
-//         />
-//         <Column
-//           dataField="purchaseCost"
-//           caption="Purchase Cost"
-//           dataType="number"
-//         />
-//         <Column dataField="orderNo" caption="Order No" dataType="string" />
-//         <Column dataField="cost" caption="Cost" dataType="number" />
-//         <Column dataField="imageId" caption="Image">
-//           <Lookup dataSource={images} displayExpr="title" valueExpr="id" />
-//         </Column>
-//         <Column dataField="categoryId" caption="Category">
-//           <Lookup dataSource={categories} displayExpr="name" valueExpr="id" />
-//         </Column>
-//         <Column dataField="conseptId" caption="Consept">
-//           <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
-//         </Column>
-//         <Column dataField="locationId" caption="Location">
-//           <Lookup dataSource={locations} displayExpr="name" valueExpr="id" />
-//         </Column>
-//         <Column dataField="companyId" caption="Company">
-//           <Lookup dataSource={companies} displayExpr="name" valueExpr="id" />
-//         </Column>
-//         <Column dataField="modelNo" caption="Model No" dataType="string" />
-//         <Column dataField="itemNo" caption="Item No" dataType="string" />
-//         <Column dataField="tag" caption="Tag" dataType="number" />
-//         <Column dataField="note" caption="Note" dataType="string" />
-//         <Selection mode="multiple" />
-//         <Toolbar>
-//           <Item location="before">
-//             <Button onClick={deleteRecords} icon="trash" text="Delete Items" />
-//           </Item>
-//           <Item name="addRowButton" showText="always" />
-//           <Item location="after">
-//             <Button icon="refresh" onClick={refreshDataGrid} />
-//           </Item>
-//           <Item name="exportButton" />
-//         </Toolbar>
-//         <Export enabled={true} allowExportSelectedData={true} />
-//         <ColumnFixing enabled={true} />
-//       </DataGrid>
-//     </React.Fragment>
-//   );
-// };
-// export default Consumable;
-import React from 'react'
+import React, { useState } from "react";
+import { modals } from "@mantine/modals";
+import TestForm from "../../components/form/TestForm";
+import ConsumableForm from "../../components/form/product/consumable/ConsumableForm";
+import GridTable from "../../components/gridTable/GridTable";
+import CustomPopup from "../../components/popup/CustomPopup";
+import { IConsumable } from "../../interfaces/interfaces";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { clearConsumable, setConsumable } from "../../redux/consumableReducer";
+import { RootState } from "../../redux/store";
 
 const Consumable = () => {
-  return (
-    <div>
-      Consumable
-    </div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const [formVisible, setFormVisible] = useState<boolean>(false);
+  const consumables = useAppSelector(
+    (state: RootState) => state.consumable.consumables
+  );
+  const categories = useAppSelector(
+    (state: RootState) => state.category.categories
+  );
 
-export default Consumable
+  const getCategoryById: React.FC<{ value: string }> = ({ value }) => {
+    const category = categories.find((category) => category.id === value);
+    return <div>{category?.name}</div>;
+  };
+  const columns = [
+    {
+      dataField: "categoryId",
+      caption: "Category",
+      dataType: "string",
+      renderComponent: getCategoryById,
+    },
+    {
+      dataField: "locationId",
+      caption: "Location",
+      dataType: "string",
+    },
+    {
+      dataField: "companyId",
+      caption: "Company",
+      dataType: "string",
+    },
+    {
+      dataField: "statusId",
+      caption: "Status",
+      dataType: "string",
+    },
+    {
+      dataField: "name",
+      caption: "Name",
+      dataType: "string",
+    },
+    { dataField: "serialNo", caption: "Serial No", dataType: "string" },
+    { dataField: "orderNo", caption: "Order No", dataType: "string" },
+    {
+      dataField: "modelNo",
+      caption: "Model No",
+      dataType: "string",
+    },
+    {
+      dataField: "itemNo",
+      caption: "Item No",
+      dataType: "string",
+    },
+    { dataField: "tagNo", caption: "Tag No", dataType: "string" },
+    {
+      dataField: "purchaseCost",
+      caption: "Purchase Cost",
+      dataType: "number",
+    },
+    {
+      dataField: "quantity",
+      caption: "Quantity",
+      dataType: "number",
+    },
+    { dataField: "purchaseDate", caption: "Purchase Date", dataType: "date" },
+    { dataField: "notes", caption: "Notes", dataType: "string" },
+  ];
+
+  const handleFormVisible = () => {
+    setFormVisible((prevFormVisible) => !prevFormVisible);
+  };
+  const onStartEdit = (row: object) => {
+    dispatch(setConsumable(row as IConsumable));
+  };
+  const onRowInsert = () => {
+    console.log("insert");
+    clearConsumable();
+    openConsumableModal();
+  };
+  const onRowUpdate = (row: object) => {
+    console.log(row);
+    openConsumableModal(row as IConsumable);
+  };
+  const onRowDelete = (row: object) => {
+    console.log("delete", row);
+  };
+  const handleUpdate = (data: object) => {
+    console.log("updateSubmit", data);
+  };
+
+  const openConsumableModal = (consumable?: IConsumable) =>
+    modals.open({
+      modalId: "consumable-modal",
+      title: "Update",
+      children: (
+        <ConsumableForm consumable={consumable} submitFunc={handleUpdate} />
+      ),
+    });
+
+  return (
+    <div
+      className="datagrid-wrapper"
+      style={{ display: "flex", flexDirection: "column", gap: "2rem" }}
+    >
+      <GridTable
+        data={consumables}
+        columns={columns}
+        hasColumnLines={false}
+        cellCssClass="testClass"
+        pageSizes={[1, 2, 5]}
+        enableEdit={true}
+        showPageSize={true}
+        onRowInsert={onRowInsert}
+        onRowUpdate={onRowUpdate}
+        onRowDelete={onRowDelete}
+        onStartEdit={onStartEdit}
+      />
+      <CustomPopup
+        visible={formVisible}
+        title="Custom Form"
+        showTitle={true}
+        showCloseButton={true}
+        dragEnabled={false}
+        height={"fit-content"}
+        width={300}
+        hideOnOutsideClick={false}
+        handleClose={handleFormVisible}
+        renderContent={() => (
+          <TestForm submitFunc={handleUpdate} columns={columns} />
+        )}
+      />
+    </div>
+  );
+};
+
+export default Consumable;
