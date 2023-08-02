1dd04195ae5862742bb7200f2af6e15fa107a796
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-02 13:43:34 +0300

    Remove Unnecessary Files

--- client/stocklinx app/src/components/generic/Company.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Company = () => {
-  return <div></div>;
-};
-
-export default Company;
--- client/stocklinx app/src/components/generic/Manufacturer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Manufacturer = () => {
-  return <div></div>;
-};
-
-export default Manufacturer;
