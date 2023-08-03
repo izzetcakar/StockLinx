c802d4010e7f5b8d9c6e2aecccb26872ceeae551
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-03 16:25:13 +0300

    Add Mantine Notifications

--- client/stocklinx app/src/main.tsx
+++ client/stocklinx app/src/main.tsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
+import { Notifications } from '@mantine/notifications';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <MantineProvider>
+    <MantineProvider withNormalizeCSS withGlobalStyles>
       <ModalsProvider>
+        <Notifications position="top-center" />
         <App />
       </ModalsProvider>
     </MantineProvider>
