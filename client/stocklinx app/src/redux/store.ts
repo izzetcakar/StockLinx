3e0dece94576e674369dbb8f8e515d98beb70730
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-04 16:21:37 +0300

    Update store.ts

--- client/stocklinx app/src/redux/store.ts
+++ client/stocklinx app/src/redux/store.ts
@@ -1,48 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
-import { createStore } from "redux";
-import userReducer from "./userReducer";
-import categoryReducer from "./categoryReducer";
-import companyReducer from "./companyReducer";
-import departmentReducer from "./departmentReducer";
-import locationReducer from "./locationReducer";
-import manufacturerReducer from "./manufacturerReducer";
-import modelReducer from "./modelReducer";
-// import productReducer from "./productReducer";
-import supplierReducer from "./supplierReducer";
-import accessoryReducer from "./accessoryReducer";
-import assetReducer from "./assetReducer";
-import componentReducer from "./componentReducer";
-import licenseReducer from "./licenseReducer";
-import consumableReducer from "./consumableReducer";
-import productStatusReducer from "./productStatusReducer";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+import accessoryReducer from "./reducers/accessory-reducer";
+import { rootSaga } from "./rootSaga";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-    accessory: accessoryReducer,
-    asset: assetReducer,
-    component: componentReducer,
-    license: licenseReducer,
-    consumable: consumableReducer,
-    category: categoryReducer,
-    company: companyReducer,
-    department: departmentReducer,
-    location: locationReducer,
-    manufacturer: manufacturerReducer,
-    model: modelReducer,
-    supplier: supplierReducer,
-    productStatus: productStatusReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+const reducer = combineReducers({
+  accessory: accessoryReducer,
 });
-//const store = createStore(combineReducers({ page: pageReducer }));
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch;
+const store = createStore(reducer, {}, applyMiddleware(...middleware));
+
+sagaMiddleware.run(rootSaga);
+
 export default store;
