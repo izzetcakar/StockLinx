7b1f90b3effe6a838fa67d9ead33d45a7aa60617
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-10 11:14:11 +0300

    Update ImageHandler.cs

--- server/StockLinx.Core/Entities/ImageHandler.cs
+++ server/StockLinx.Core/Entities/ImageHandler.cs
@@ -15,5 +15,15 @@
 
             System.IO.File.WriteAllBytes(filePath, imageBytes);
         }
+        public static string GetBase64FromFile(string fileName)
+        {
+            string uploadDir = Path.Combine(Directory.GetCurrentDirectory(), "Resources/Images");
+            string filePath = Path.Combine(uploadDir, fileName);
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(filePath);
+            string base64String = Convert.ToBase64String(imageBytes);
+
+            return base64String;
+        }
     }
 }
