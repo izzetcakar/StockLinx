e269ab9d2e7761f970b4f2ca6abab115e8a4239a
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-10 11:15:08 +0300

    Create ImageController.cs

---
+++ server/StockLinx.API/Controllers/ImageController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockLinx.Core.DTOs.Others;
+using StockLinx.Core.Entities;
+
+namespace StockLinx.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : CustomBaseController
+    {
+        [HttpGet("{path}")]
+        public async Task<IActionResult> Get(string path)
+        {
+            var base64Image = ImageHandler.GetBase64FromFile(path);
+            return CreateActionResult(CustomResponseDto<string>.Success(200, base64Image));
+        }
+    }
+}
