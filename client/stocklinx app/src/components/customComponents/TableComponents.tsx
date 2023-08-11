5c903408a70299c64f30fbce18c642489f04ce67
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-11 10:45:25 +0300

    Update TableComponents.tsx

--- client/stocklinx app/src/components/customComponents/TableComponents.tsx
+++ client/stocklinx app/src/components/customComponents/TableComponents.tsx
@@ -1,77 +1,12 @@
-import { useSelector } from "react-redux";
 import { getNameFromArray } from "../../functions/getNameFromArray";
-import { RootState } from "../../redux/rootReducer";
+import { BaseEntity } from "../../interfaces/interfaces";
 
 export interface IValue {
     value: string | number | boolean | null;
 }
 
-// export const NameComponent: React.FC<{ value: any, data: BaseEntity[] }> = ({ value = "", data = [] }) => {
-//     const name = getNameFromArray(data, value as string);
-//     return (
-//         <div>
-//             {name}
-//         </div>
-//     );
-// };
-
-export const CategoryNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const categories = useSelector((state: RootState) => state.category.categories);
-    const name = getNameFromArray(categories, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const LocationNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const locations = useSelector((state: RootState) => state.location.locations);
-    const name = getNameFromArray(locations, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const CompanyNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const companies = useSelector((state: RootState) => state.company.companies);
-    const name = getNameFromArray(companies, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const ManufacturerNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const manufacturers = useSelector((state: RootState) => state.manufacturer.manufacturers);
-    const name = getNameFromArray(manufacturers, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const SupplierNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const suppliers = useSelector((state: RootState) => state.supplier.suppliers);
-    const name = getNameFromArray(suppliers, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const StatusNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const productStatuses = useSelector((state: RootState) => state.productStatus.productStatuses);
-    const name = getNameFromArray(productStatuses, value?.toString());
-    return (
-        <div>
-            {name}
-        </div>
-    );
-};
-export const ModelNameComponent: React.FC<{ value: IValue }> = ({ value = "" }) => {
-    const models = useSelector((state: RootState) => state.model.models);
-    const name = getNameFromArray(models, value?.toString());
+export const NameComponent: (value: string, data: BaseEntity[]) => JSX.Element = (value, data) => {
+    const name = getNameFromArray(data, value?.toString());
     return (
         <div>
             {name}
