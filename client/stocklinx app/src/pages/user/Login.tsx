ad6ef276fd805ef62e399809648f218a8dfea6ea
İzzet Çakar <izzetcakar4@gmail.com>
2023-08-11 10:45:46 +0300

    Update Login.tsx

--- client/stocklinx app/src/pages/user/Login.tsx
+++ client/stocklinx app/src/pages/user/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import logo from "/logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "@mantine/form";
