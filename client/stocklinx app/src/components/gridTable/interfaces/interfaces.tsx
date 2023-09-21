5b181b31076d4fe12677e5d4fbfe9848cdf9f925
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-21 17:13:29 +0300

    Column updated

--- client/stocklinx app/src/components/gridTable/interfaces/interfaces.tsx
+++ client/stocklinx app/src/components/gridTable/interfaces/interfaces.tsx
@@ -1,7 +1,6 @@
 export interface Column {
     dataField: string;
     caption: string;
-    dataType: string;
     renderComponent?: (value: any) => JSX.Element;
 }
 export interface IDropdownData {
