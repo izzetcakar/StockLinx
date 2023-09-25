1b41ee9a9d0ee862c80ada600d42d38f5688c44c
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-25 11:33:22 +0300

    Update interfaces.tsx

--- client/stocklinx app/src/forms/interfaces/interfaces.tsx
+++ client/stocklinx app/src/forms/interfaces/interfaces.tsx
@@ -1,11 +1,15 @@
+import { UseFormReturnType } from "@mantine/form";
+
 export interface IMantineSelectData
   extends React.ComponentPropsWithoutRef<"div"> {
   value: string;
   label: string;
 }
-export interface IMantinSelectProps {
+export interface IMantinSelectProps<T> {
   data: IMantineSelectData[];
   value: string;
   label: string;
   propTag: string;
+  clearable?: boolean;
+  form?: UseFormReturnType<T>;
 }
