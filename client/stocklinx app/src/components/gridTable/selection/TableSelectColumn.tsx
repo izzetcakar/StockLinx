6bea296915a35b7c829fe0d017c006a7a925ec2d
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-26 11:37:01 +0300

    Update TableSelectColumn.tsx

--- client/stocklinx app/src/components/gridTable/selection/TableSelectColumn.tsx
+++ client/stocklinx app/src/components/gridTable/selection/TableSelectColumn.tsx
@@ -1,59 +1,59 @@
-import TableCheckbox from './TableCheckbox'
-import { hasAllElements } from '../../../functions/hasAllElements'
-import { getIndexesFromArray } from '../../../functions/getIndexesFromArray'
+import TableCheckbox from "./TableCheckbox";
+import { hasAllElements } from "../../../functions/hasAllElements";
+import { getIndexesFromArray } from "../../../functions/getIndexesFromArray";
 
 interface TableSelectColumnProps {
-    datagrid: object[];
-    selectedIndexes: number[];
-    filterData: () => object[];
-    setSelectedIndexes: React.Dispatch<React.SetStateAction<number[]>>;
-    handleClassByIndex: (index: number) => string;
+  data: object[];
+  selectedIndexes: number[];
+  filterData: () => object[];
+  setSelectedIndexes: React.Dispatch<React.SetStateAction<number[]>>;
+  handleClassByIndex: (index: number) => string;
 }
 
 const TableSelectColumn: React.FC<TableSelectColumnProps> = ({
-    datagrid,
-    selectedIndexes,
-    filterData,
-    setSelectedIndexes,
-    handleClassByIndex
+  data,
+  selectedIndexes,
+  filterData,
+  setSelectedIndexes,
+  handleClassByIndex,
 }) => {
-    const handleSelectRow = (index: number) => {
-        setSelectedIndexes((prevIndexes) =>
-            prevIndexes.includes(index)
-                ? prevIndexes.filter((i) => i !== index)
-                : [...prevIndexes, index]
-        );
-    };
+  const handleSelectRow = (index: number) => {
+    setSelectedIndexes((prevIndexes) =>
+      prevIndexes.includes(index)
+        ? prevIndexes.filter((i) => i !== index)
+        : [...prevIndexes, index]
+    );
+  };
 
-    const handleSelectAll = () => {
-        setSelectedIndexes((prevIndexes) =>
-            hasAllElements(getIndexesFromArray(filterData()), prevIndexes)
-                ? []
-                : getIndexesFromArray(datagrid)
-        );
-    };
+  const handleSelectAll = () => {
+    setSelectedIndexes((prevIndexes) =>
+      hasAllElements(getIndexesFromArray(filterData()), prevIndexes)
+        ? []
+        : getIndexesFromArray(data)
+    );
+  };
 
-    return (
-        <div className="column-container">
-            <div className="column-title">
-                <TableCheckbox
-                    isChecked={hasAllElements(
-                        getIndexesFromArray(filterData()),
-                        selectedIndexes
-                    )}
-                    selectFunc={handleSelectAll}
-                />
-            </div>
-            {getIndexesFromArray(filterData()).map((_, index) => (
-                <div className={handleClassByIndex(index)} key={index}>
-                    <TableCheckbox
-                        isChecked={selectedIndexes.includes(index)}
-                        selectFunc={() => handleSelectRow(index)}
-                    />
-                </div>
-            ))}
+  return (
+    <div className="table-column">
+      <div className="table-column-title">
+        <TableCheckbox
+          isChecked={hasAllElements(
+            getIndexesFromArray(filterData()),
+            selectedIndexes
+          )}
+          selectFunc={handleSelectAll}
+        />
+      </div>
+      {getIndexesFromArray(filterData()).map((_, index) => (
+        <div className={handleClassByIndex(index)} key={index}>
+          <TableCheckbox
+            isChecked={selectedIndexes.includes(index)}
+            selectFunc={() => handleSelectRow(index)}
+          />
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default TableSelectColumn
+export default TableSelectColumn;
