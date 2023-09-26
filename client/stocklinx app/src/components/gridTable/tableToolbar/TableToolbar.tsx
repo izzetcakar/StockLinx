1b7c669bce9a5e7c648b8ab875db86932b204fb6
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-26 11:36:54 +0300

    Update Table Toolbar

--- client/stocklinx app/src/components/gridTable/tableToolbar/TableToolbar.tsx
+++ client/stocklinx app/src/components/gridTable/tableToolbar/TableToolbar.tsx
@@ -1,33 +1,42 @@
-import React from 'react'
+import React from "react";
 import "./tableToolbar.scss";
-import { Column } from '../interfaces/interfaces';
-import Dropdown from './Dropdown';
+import { Column } from "../interfaces/interfaces";
+import Dropdown from "./Dropdown";
 import icon_plus from "../../.././assets/icon_plus.png";
 import icon_refresh_outlined from "../../.././assets/icon_refresh_outlined.png";
-import ActionIconBtn from '../../generic/ActionIconBtn';
+import ActionIconBtn from "../../generic/ActionIconBtn";
 
 interface TableToolbarProps {
-    refreshData?: () => Promise<void> | void;
-    filterData?: () => void;
-    handleSearch?: () => void;
-    columns: Column[];
-    visibleColumns: string[];
-    handleVisibleColumns: (columnCaption: string) => void;
-    onRowInsert?: () => void;
+  refreshData?: () => Promise<void> | void;
+  filterData?: () => void;
+  handleSearch?: () => void;
+  columns: Column[];
+  visibleColumns: string[];
+  addVisibleColumn: (column: string) => void;
+  onRowInsert?: () => void;
 }
-const TableToolbar: React.FC<TableToolbarProps> = ({ columns, visibleColumns, refreshData, handleVisibleColumns, onRowInsert }) => {
+const TableToolbar: React.FC<TableToolbarProps> = ({
+  columns,
+  visibleColumns,
+  refreshData,
+  addVisibleColumn,
+  onRowInsert,
+}) => {
+  return (
+    <div className="table-toolbar">
+      <Dropdown
+        columns={columns}
+        onChange={addVisibleColumn}
+        visibleColumns={visibleColumns}
+      />
+      {onRowInsert ? (
+        <ActionIconBtn submitFunc={onRowInsert} icon={icon_plus} />
+      ) : null}
+      {refreshData ? (
+        <ActionIconBtn submitFunc={refreshData} icon={icon_refresh_outlined} />
+      ) : null}
+    </div>
+  );
+};
 
-    return (
-        <div className="table-toolbar-container">
-            <div className="table-toolbar-element">
-                <div className="table-toolbar-element-content">
-                    <Dropdown columns={columns} onChange={handleVisibleColumns} visibleColumns={visibleColumns} />
-                </div>
-            </div>
-            {onRowInsert ? <ActionIconBtn submitFunc={onRowInsert} icon={icon_plus} /> : null}
-            {refreshData ? <ActionIconBtn submitFunc={refreshData} icon={icon_refresh_outlined} /> : null}
-        </div>
-    )
-}
-
-export default TableToolbar
+export default TableToolbar;
