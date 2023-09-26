d1e50a23583e156eb077f9a2e05dfae7bcdf884f
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-26 15:10:28 +0300

    Update Test.tsx

--- client/stocklinx app/src/pages/Test.tsx
+++ client/stocklinx app/src/pages/Test.tsx
@@ -68,6 +68,111 @@ const Test = () => {
       contact: "Marie Bertrand",
       country: "France",
     },
+    {
+      company: "Ernst Handel",
+      contact: "Roland Mendel",
+      country: "Austria",
+    },
+    {
+      company: "Island Trading",
+      contact: "Helen Bennett",
+      country: "UK",
+    },
+    {
+      company: "Königlich Essen",
+      contact: "Philip Cramer",
+      country: "Germany",
+    },
+    {
+      company: "Laughing Bacchus Winecellars",
+      contact: "Yoshi Tannamuri",
+      country: "Canada",
+    },
+    {
+      company: "Magazzini Alimentari Riuniti",
+      contact: "Giovanni Rovelli",
+      country: "Italy",
+    },
+    {
+      company: "North/South",
+      contact: "Simon Crowther",
+      country: "UK",
+    },
+    {
+      company: "Paris spécialités",
+      contact: "Marie Bertrand",
+      country: "France",
+    },
+    {
+      company: "Ernst Handel",
+      contact: "Roland Mendel",
+      country: "Austria",
+    },
+    {
+      company: "Island Trading",
+      contact: "Helen Bennett",
+      country: "UK",
+    },
+    {
+      company: "Königlich Essen",
+      contact: "Philip Cramer",
+      country: "Germany",
+    },
+    {
+      company: "Laughing Bacchus Winecellars",
+      contact: "Yoshi Tannamuri",
+      country: "Canada",
+    },
+    {
+      company: "Magazzini Alimentari Riuniti",
+      contact: "Giovanni Rovelli",
+      country: "Italy",
+    },
+    {
+      company: "North/South",
+      contact: "Simon Crowther",
+      country: "UK",
+    },
+    {
+      company: "Paris spécialités",
+      contact: "Marie Bertrand",
+      country: "France",
+    },
+    {
+      company: "Ernst Handel",
+      contact: "Roland Mendel",
+      country: "Austria",
+    },
+    {
+      company: "Island Trading",
+      contact: "Helen Bennett",
+      country: "UK",
+    },
+    {
+      company: "Königlich Essen",
+      contact: "Philip Cramer",
+      country: "Germany",
+    },
+    {
+      company: "Laughing Bacchus Winecellars",
+      contact: "Yoshi Tannamuri",
+      country: "Canada",
+    },
+    {
+      company: "Magazzini Alimentari Riuniti",
+      contact: "Giovanni Rovelli",
+      country: "Italy",
+    },
+    {
+      company: "North/South",
+      contact: "Simon Crowther",
+      country: "UK",
+    },
+    {
+      company: "Paris spécialités",
+      contact: "Marie Bertrand",
+      country: "France",
+    },
   ];
   return <GridTable columns={columns} data={data} />;
 };
