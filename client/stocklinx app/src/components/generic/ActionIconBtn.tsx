e38ddecfbb3806b2791b4e2fbe1011554e5e664c
İzzet Çakar <izzetcakar4@gmail.com>
2023-09-28 14:27:31 +0300

    ActionIconBtn Updated

--- client/stocklinx app/src/components/generic/ActionIconBtn.tsx
+++ client/stocklinx app/src/components/generic/ActionIconBtn.tsx
@@ -1,23 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./generic.scss";
 
 interface ActionIconBtnProps {
-    submitFunc: () => void;
-    icon: string;
+  submitFunc: () => void;
+  icon: string;
+  iconSize?: number;
+  disable?: boolean;
 }
-const ActionIconBtn: React.FC<ActionIconBtnProps> = ({ submitFunc, icon }) => {
-    const [clicked, setClicked] = useState(false);
+const ActionIconBtn: React.FC<ActionIconBtnProps> = ({
+  submitFunc,
+  icon,
+  iconSize = 18,
+  disable = false,
+}) => {
+  const [clicked, setClicked] = useState(false);
 
-    const handleClick = () => {
-        submitFunc();
-        setClicked(true);
-        setTimeout(() => setClicked(false), 100); // Reset the clicked state after 100ms
-    };
-    return (
-        <button type='submit' className={clicked ? 'action-btn clicked' : 'action-btn'} onClick={handleClick}>
-            <img className='action-btn-icon' src={icon}></img>
-        </button>
-    )
-}
+  const handleClick = () => {
+    submitFunc();
+    setClicked(true);
+    setTimeout(() => setClicked(false), 100);
+  };
+  return (
+    <button
+      type="submit"
+      className={clicked ? "action-btn clicked" : "action-btn"}
+      onClick={disable ? () => {} : handleClick}
+    >
+      <img
+        className={disable ? "action-btn-icon disable" : "action-btn-icon"}
+        src={icon}
+        style={{ width: iconSize, height: iconSize }}
+      ></img>
+    </button>
+  );
+};
 
-export default ActionIconBtn
+export default ActionIconBtn;
