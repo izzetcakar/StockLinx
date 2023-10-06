768580f8154b5dd9efb243d619bab8b0c70e9f59
İzzet Çakar <izzetcakar4@gmail.com>
2023-10-06 17:19:39 +0300

    Create ProductLocationCounter.cs

---
+++ server/StockLinx.Core/DTOs/Others/ProductLocationCounter.cs
@@ -0,0 +1,10 @@
+﻿namespace StockLinx.Core.DTOs.Others
+{
+    public class ProductLocationCounter
+    {
+        public Guid LocationId { get; set; }
+        public string LocationName { get; set; }
+        public int ProductCount { get; set; }
+        public int AssignedCount { get; set; }
+    }
+}
