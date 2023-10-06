1150aaaf445767093ba83a400b9aef700a694f6d
İzzet Çakar <izzetcakar4@gmail.com>
2023-10-06 17:19:27 +0300

    Update ConsumableCreateDto.cs

--- server/StockLinx.Core/DTOs/Create/ConsumableCreateDto.cs
+++ server/StockLinx.Core/DTOs/Create/ConsumableCreateDto.cs
@@ -4,8 +4,8 @@ namespace StockLinx.Core.DTOs.Create
 {
     public class ConsumableCreateDto : BaseProductCreateDto
     {
-        public int? TagNo { get; set; }
-        public string? ModelNo { get; set; }
+        public int Quantity { get; set; }
         public string? ItemNo { get; set; }
+        public string? ModelNo { get; set; }
     }
 }
