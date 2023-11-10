c57d1f09f385032f1d9ad2cb4e178de69610c93a
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2023-11-10 23:30:18 +0300

    Update main.tsx

--- client/stocklinx app/src/main.tsx
+++ client/stocklinx app/src/main.tsx
@@ -9,17 +9,31 @@ import { Notifications } from "@mantine/notifications";
 import { MantineProvider } from "@mantine/core";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <ModalsProvider>
-      <Notifications w={"fit-content"} />
-      <MantineProvider
-        theme={{
-          primaryShade: 9,
-          cursorType: "pointer",
-        }}
-      >
+  <MantineProvider
+    theme={{
+      primaryShade: 9,
+      cursorType: "pointer",
+      radius: {
+        xs: "2px",
+        sm: "2px",
+        md: "2px",
+        lg: "2px",
+        xl: "2px",
+      },
+      fontSizes: {
+        xs: "13px",
+        sm: "13px",
+        md: "13px",
+        lg: "13px",
+        xl: "13px",
+      },
+    }}
+  >
+    <Provider store={store}>
+      <ModalsProvider>
+        <Notifications w={"fit-content"} />
         <App />
-      </MantineProvider>
-    </ModalsProvider>
-  </Provider>
+      </ModalsProvider>
+    </Provider>
+  </MantineProvider>
 );
