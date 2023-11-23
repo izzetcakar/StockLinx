9aeab179f7bc02d6a9f7fe804c368304d9cd7160
İzzet Çakar <izzetcakar4@gmail.com>
2023-11-23 16:18:42 +0300

    update model

--- client/stocklinx app/src/forms/model/ModelForm.tsx
+++ client/stocklinx app/src/forms/model/ModelForm.tsx
@@ -3,41 +3,42 @@ import {
   TextInput,
   Button,
   Group,
-  FileInput,
-  rem,
-  Image,
-  ScrollArea,
   Flex,
   Textarea,
+  Select,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
-import { closeModal } from "@mantine/modals";
-import { IconUpload } from "@tabler/icons-react";
-import { modals } from "@mantine/modals";
 import { IModel } from "../../interfaces/interfaces";
-import { handleImageChange } from "../functions/formFunctions";
-import MantineSelect from "../components/MantineSelect";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { modelActions } from "../../redux/model/actions";
-import { useSelectData } from "./selectData";
-
+import uuid4 from "uuid4";
+import filterClasses from "../../mantineModules/baseFilter.module.scss";
+import { RootState } from "../../redux/rootReducer";
 interface ModelFormProps {
   model?: IModel;
 }
 
 const ModelForm: React.FC<ModelFormProps> = ({ model }) => {
   const dispatch = useDispatch();
+  const categories = useSelector(
+    (state: RootState) => state.category.categories
+  );
+  const fieldSets = useSelector((state: RootState) => state.fieldSet.fieldSets);
+  const manufacturers = useSelector(
+    (state: RootState) => state.manufacturer.manufacturers
+  );
 
   const form = useForm<IModel>({
     initialValues: model
       ? { ...model }
       : {
-          id: "",
-          categoryId: null,
-          manufacturerId: null,
+          id: uuid4(),
           name: "",
-          imagePath: null,
+          categoryId: "",
+          fieldSetId: null,
+          manufacturerId: null,
           modelNo: null,
+          imagePath: null,
           notes: null,
         },
     validate: {
@@ -51,76 +52,79 @@ const ModelForm: React.FC<ModelFormProps> = ({ model }) => {
       : dispatch(modelActions.create({ model: data as IModel }));
     dispatch(modelActions.getAll());
   };
-  const openNextModel = () =>
-    modals.open({
-      modalId: "next-modal",
-      title: "Page 2",
-      children: (
-        <Button fullWidth onClick={() => closeModal("next-modal")} color="dark">
-          Back
-        </Button>
-      ),
-    });
 
   return (
-    <ScrollArea.Autosize type="always" offsetScrollbars mah={600}>
-      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
-        <Flex direction="column" gap={10} mx="auto" maw="auto" px={40}>
-          {useSelectData(form).map((selectData) => (
-            <MantineSelect
-              data={selectData.data}
-              value={selectData.value}
-              label={selectData.label}
-              propTag={selectData.propTag}
-              key={selectData.propTag}
-              form={form}
-            />
-          ))}
-          <TextInput
-            label="Name"
-            placeholder="New Name"
-            {...form.getInputProps("name")}
-          />
-          <TextInput
-            label="Model No"
-            placeholder="Model No"
-            {...form.getInputProps("modelNo")}
-            value={form.values.modelNo || ""}
-          />
-          <FileInput
-            label="Upload image"
-            placeholder="Upload image"
-            accept="image/png,image/jpeg"
-            // value={form.getTransformedValues().imagePath}
-            onChange={(value) => void handleImageChange(form, value)}
-            icon={
-              <IconUpload size={rem(14)} {...form.getInputProps("imagePath")} />
-            }
-          />
-          <Image
-            maw={240}
-            mx="auto"
-            radius="md"
-            src={form.values.imagePath}
-            alt="Random image"
-          />
-          <Textarea
-            placeholder="Your notes here"
-            label="Note"
-            {...form.getInputProps("notes")}
-            value={form.values.notes || ""}
-          />
-          <Group position="right" mt="md">
-            <Button type="submit" color="dark">
-              Submit
-            </Button>
-            <Button onClick={() => openNextModel()} color="dark">
-              Next Modal
-            </Button>
-          </Group>
-        </Flex>
-      </form>
-    </ScrollArea.Autosize>
+    <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+      <Flex
+        direction="column"
+        gap={10}
+        mx="auto"
+        h={"70dvh"}
+        w={"80dvw"}
+        px={40}
+        pt={20}
+      >
+        <Select
+          data={categories.map((category) => ({
+            value: category.id,
+            label: category.name,
+          }))}
+          label="Category"
+          placeholder="Select Category"
+          {...form.getInputProps("categoryId")}
+          classNames={filterClasses}
+          dropdownPosition="bottom"
+          nothingFound="No category found"
+          withAsterisk
+        />
+        <Select
+          data={fieldSets.map((fieldSet) => ({
+            value: fieldSet.id,
+            label: fieldSet.name,
+          }))}
+          label="Field Set"
+          placeholder="Select Field Set"
+          {...form.getInputProps("fieldSetId")}
+          classNames={filterClasses}
+          dropdownPosition="bottom"
+          nothingFound="No field set found"
+        />
+        <Select
+          data={manufacturers.map((manufacturer) => ({
+            value: manufacturer.id,
+            label: manufacturer.name,
+          }))}
+          label="Manufacturer"
+          placeholder="Select Manufacturer"
+          {...form.getInputProps("manufacturerId")}
+          classNames={filterClasses}
+          dropdownPosition="bottom"
+          nothingFound="No manufacturer found"
+        />
+        <TextInput
+          label="Name"
+          placeholder="New Name"
+          {...form.getInputProps("name")}
+        />
+        <TextInput
+          label="Model No"
+          placeholder="Model No"
+          {...form.getInputProps("modelNo")}
+          value={form.values.modelNo || ""}
+        />
+        <Textarea
+          label="Notes"
+          placeholder="Your notes here"
+          {...form.getInputProps("notes")}
+          value={form.values.notes || ""}
+        />
+        <Group position="right" mt="md">
+          <Button type="submit" color="dark">
+            Submit
+          </Button>
+        </Group>
+      </Flex>
+    </form>
   );
 };
 
--- client/stocklinx app/src/pages/model/Model.tsx
+++ client/stocklinx app/src/pages/model/Model.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { IModel } from "../../interfaces/interfaces";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/rootReducer";
+import { useColumns } from "./columns";
+import Gridtable from "../../components/gridTable/GridTable";
+import { companyActions } from "../../redux/company/actions";
+import { modelActions } from "../../redux/model/actions";
+import { locationActions } from "../../redux/location/actions";
+import { branchActions } from "../../redux/branch/actions";
+import { openModelModal } from "../../modals/modals";
 
 const Model = () => {
-  return <div>Model Page</div>;
+  const dispatch = useDispatch();
+  const models = useSelector((state: RootState) => state.model.models);
+
+  const refreshData = () => {
+    dispatch(modelActions.getAll());
+    dispatch(locationActions.getAll());
+    dispatch(companyActions.getAll());
+    dispatch(branchActions.getAll());
+  };
+
+  return (
+    <>
+      <div className="page-content-header">
+        <div className="page-content-header-title">Models</div>
+      </div>
+      <Gridtable
+        data={models}
+        itemKey="id"
+        columns={useColumns().columns}
+        refreshData={refreshData}
+        onRowUpdate={(model) => openModelModal(model as IModel)}
+        onRowInsert={() => openModelModal()}
+        onRowRemove={(id) => dispatch(modelActions.remove({ id: id }))}
+        onRowRemoveRange={(ids) =>
+          dispatch(modelActions.removeRange({ ids: ids }))
+        }
+        excelColumns={useColumns().excelColumns}
+        enableToolbar
+        enableEditActions
+        enableExcelActions
+        enableSelectActions
+      />
+    </>
+  );
 };
 
 export default Model;
