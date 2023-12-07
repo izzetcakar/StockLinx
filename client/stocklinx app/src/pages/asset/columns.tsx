edc7a02cb2ab461d8aa8bae7853dd41a131e7279
İzzet Çakar <izzetcakar4@gmail.com>
2023-12-07 16:00:35 +0300

    checkin/checkout is added to asset columns

--- client/stocklinx app/src/pages/asset/columns.tsx
+++ client/stocklinx app/src/pages/asset/columns.tsx
@@ -1,16 +1,44 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../redux/rootReducer";
 import {
   ExcelColumn,
   Column,
 } from "../../components/gridTable/interfaces/interfaces";
+import { Button } from "@mantine/core";
+import { openConfirmModal } from "../../components/gridTable/modals/modals";
+import { deployedProductActions } from "../../redux/deployedProduct/actions";
+import { openCheckInModal } from "../../modals/modals";
+import { IDeployedProduct } from "../../interfaces/interfaces";
+import uuid4 from "uuid4";
 
 export const useColumns = () => {
+  const dispatch = useDispatch();
   const branches = useSelector((state: RootState) => state.branch.branches);
   const models = useSelector((state: RootState) => state.model.models);
   const productStatuses = useSelector(
     (state: RootState) => state.productStatus.productStatuses
   );
+  const users = useSelector((state: RootState) => state.user.users);
+  const deployedProducts = useSelector(
+    (state: RootState) => state.deployedProduct.deployedProducts
+  );
+  const checkOut = (id: string) => {
+    dispatch(deployedProductActions.remove({ id: id }));
+  };
+  const checkIn = (id: string) => {
+    const newDeployedProduct: IDeployedProduct = {
+      id: uuid4(),
+      userId: "",
+      assetId: id,
+      licenseId: null,
+      accessoryId: null,
+      componentId: null,
+      consumableId: null,
+      assignDate: new Date(),
+      notes: null,
+    };
+    openCheckInModal(newDeployedProduct);
+  };
 
   const columns: Column[] = [
     {
@@ -48,11 +76,53 @@ export const useColumns = () => {
       },
       dataType: "string",
     },
-    // ADD ASSIGNED USER
+    {
+      dataField: "id",
+      caption: "Checked Out To",
+      dataType: "string",
+      renderComponent(value) {
+        const deployedProduct = deployedProducts.find(
+          (deployedProduct) => deployedProduct.assetId === value
+        );
+        if (deployedProduct) {
+          const user = users.find((user) => user.id === deployedProduct.userId);
+          if (user) return user.firstName + " " + user.lastName;
+        }
+        return "";
+      },
+    },
     {
       dataField: "purchaseCost",
       caption: "Purchase Cost",
       dataType: "number",
+    },
+    {
+      dataField: "id",
+      caption: "Checkin/Checkout",
+      dataType: "action",
+      renderComponent(value) {
+        const deployedProduct = deployedProducts.find(
+          (deployedProduct) => deployedProduct.assetId === value
+        );
+        return (
+          <Button
+            color={deployedProduct ? "red" : "green"}
+            variant="filled"
+            size="xs"
+            onClick={() =>
+              deployedProduct
+                ? openConfirmModal(
+                    "Check Out",
+                    "Are you sure you want to check out this asset",
+                    () => checkOut(deployedProduct.id)
+                  )
+                : checkIn(value)
+            }
+          >
+            {deployedProduct ? "Check Out" : "Check In"}
+          </Button>
+        );
+      },
     },
     // INVISIBLE COLUMNS
     {
