c3f2e63e844135df173c72ef110c4c3d785dca45
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2023-12-16 00:54:27 +0300

    Update UserService.cs

--- server/StockLinx.Service/Services/UserService.cs
+++ server/StockLinx.Service/Services/UserService.cs
@@ -17,22 +17,26 @@ namespace StockLinx.Service.Services
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IUserRepository _userRepository;
+        private readonly ICustomLogService _customLogService;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
 
         public UserService(IRepository<User> repository, IUnitOfWork unitOfWork, IUserRepository userRepository,
-             IMapper mapper, IHttpContextAccessor httpContextAccessor) : base(repository, unitOfWork)
+             IMapper mapper, IHttpContextAccessor httpContextAccessor, ICustomLogService customLogService) : base(repository, unitOfWork)
         {
             _httpContextAccessor = httpContextAccessor;
             _userRepository = userRepository;
             _mapper = mapper;
             _unitOfWork = unitOfWork;
+            _customLogService = customLogService;
         }
+
         public async Task<UserDto> GetDto(Guid id)
         {
             var user = await GetByIdAsync(id);
             return _userRepository.GetDto(user);
         }
+
         public async Task<List<UserDto>> GetAllDtos()
         {
             return await _userRepository.GetAllDtos();
@@ -57,6 +61,7 @@ namespace StockLinx.Service.Services
                 throw new NotImplementedException("Invalid Token");
             }
         }
+
         public async Task<User> GetCurrentUser()
         {
             var userId = GetIdByToken();
@@ -70,45 +75,15 @@ namespace StockLinx.Service.Services
                 return user;
             }
         }
+
         public async Task<User> Login(UserLoginDto userLoginDto)
         {
-
-            var isExist = await _userRepository.AnyAsync(x => x.Email == userLoginDto.Email && x.Password == userLoginDto.Password);
-            if (!isExist)
+            var user = await _userRepository.Where(x => x.Email == userLoginDto.Email && x.Password == userLoginDto.Password).SingleOrDefaultAsync();
+            if (user == null)
             {
                 throw new Exception("User is not found");
             }
-            else
-            {
-                return await _userRepository.Where(x => x.Email == userLoginDto.Email && x.Password == userLoginDto.Password).SingleOrDefaultAsync();
-            }
-        }
-
-        public Task Logout()
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<User> Register(UserCreateDto createUser)
-        {
-            var user = _mapper.Map<User>(createUser);
-            var userId = Guid.NewGuid();
-            user.Id = userId;
-            user.CreatedDate = DateTime.UtcNow;
-            user.IsAdmin = false;
-            var employeeNoExist = await _userRepository.AnyAsync(x => x.EmployeeNo == user.EmployeeNo);
-
-            if (employeeNoExist)
-            {
-                throw new Exception("EmployeeNo already exists");
-            }
-            else
-            {
-                await AddAsync(user);
-                await _unitOfWork.CommitAsync();
-                var returnedUser = await _userRepository.Where(x => x.Id == userId).SingleOrDefaultAsync();
-                return returnedUser;
-            }
+            return user;
         }
 
         public async Task<UserDto> CreateUserAsync(UserCreateDto createDto)
@@ -116,7 +91,14 @@ namespace StockLinx.Service.Services
             var newUser = _mapper.Map<User>(createDto);
             newUser.Id = Guid.NewGuid();
             newUser.CreatedDate = DateTime.UtcNow;
+            newUser.IsAdmin = false;
+            var employeeNoExist = await _userRepository.AnyAsync(x => x.EmployeeNo == newUser.EmployeeNo);
+            if (employeeNoExist)
+            {
+                throw new Exception("EmployeeNo already exists");
+            }
             await _userRepository.AddAsync(newUser);
+            await _customLogService.CreateCustomLog("Create", newUser.Id, newUser.DepartmentId, "User", "Department");
             await _unitOfWork.CommitAsync();
             return _userRepository.GetDto(newUser);
         }
@@ -129,7 +111,14 @@ namespace StockLinx.Service.Services
                 var newUser = _mapper.Map<User>(createDto);
                 newUser.Id = Guid.NewGuid();
                 newUser.CreatedDate = DateTime.UtcNow;
+                newUser.IsAdmin = false;
+                var employeeNoExist = await _userRepository.AnyAsync(x => x.EmployeeNo == newUser.EmployeeNo);
+                if (employeeNoExist)
+                {
+                    throw new Exception("EmployeeNo already exists");
+                }
                 newUsers.Add(newUser);
+                await _customLogService.CreateCustomLog("Create", newUser.Id, newUser.DepartmentId, "User", "Department");
             }
             await _userRepository.AddRangeAsync(newUsers);
             await _unitOfWork.CommitAsync();
@@ -146,6 +135,7 @@ namespace StockLinx.Service.Services
             var updatedUser = _mapper.Map<User>(updateDto);
             updatedUser.UpdatedDate = DateTime.UtcNow;
             _userRepository.Update(userInDb, updatedUser);
+            await _customLogService.CreateCustomLog("Update", updatedUser.Id, updatedUser.DepartmentId, "User", "Department");
             await _unitOfWork.CommitAsync();
             return _userRepository.GetDto(updatedUser);
         }
@@ -159,6 +149,7 @@ namespace StockLinx.Service.Services
             }
             user.DeletedDate = DateTime.UtcNow;
             _userRepository.Update(user, user);
+            await _customLogService.CreateCustomLog("Delete", user.Id, user.DepartmentId, "User", "Department");
             await _unitOfWork.CommitAsync();
         }
 
@@ -174,6 +165,7 @@ namespace StockLinx.Service.Services
                 }
                 user.DeletedDate = DateTime.UtcNow;
                 users.Add(user);
+                await _customLogService.CreateCustomLog("Delete", user.Id, user.DepartmentId, "User", "Department");
             }
             _userRepository.UpdateRange(users);
             await _unitOfWork.CommitAsync();
