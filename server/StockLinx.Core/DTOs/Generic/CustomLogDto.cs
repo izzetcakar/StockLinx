1b97db0a58832327e75f39088c8f1b5648c730f0
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2023-12-17 01:59:23 +0300

    Update CustomLogDto.cs

--- server/StockLinx.Core/DTOs/Generic/CustomLogDto.cs
+++ server/StockLinx.Core/DTOs/Generic/CustomLogDto.cs
@@ -6,8 +6,10 @@
         public Guid UserId { get; set; }
         public DateTime Date { get; set; }
         public string Action { get; set; }
+        public string ItemId { get; set; }
         public string ItemRoute { get; set; }
         public string ItemName { get; set; }
+        public string? TargetId { get; set; }
         public string? TargetName { get; set; }
         public string? TargetRoute { get; set; }
         public string ItemController { get; set; }
