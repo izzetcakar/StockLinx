974a9e379be8eb7559ba613c2f3039f63c1d206b
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2023-12-17 01:59:21 +0300

    Update DeployedProductDto.cs

--- server/StockLinx.Core/DTOs/Generic/DeployedProductDto.cs
+++ server/StockLinx.Core/DTOs/Generic/DeployedProductDto.cs
@@ -3,14 +3,13 @@
     public class DeployedProductDto : BaseDto
     {
         public Guid UserId { get; set; }
-        public Guid? AccessoryId { get; set; }
-        public Guid? AssetId { get; set; }
-        public Guid? ComponentId { get; set; }
-        public Guid? ConsumableId { get; set; }
-        public Guid? LicenseId { get; set; }
-        public string? Notes { get; set; }
+        public Guid ProductId { get; set; }
         public DateTime AssignDate { get; set; }
         public string? ProductName { get; set; }
+        public string? ProductType { get; set; }
+        public string? ProductRoute { get; set; }
+        public string? Category { get; set; }
         public string? ProductDescription { get; set; }
+        public string? Notes { get; set; }
     }
 }
