41c971bc91362968bc1a421f2d4c8ab14007c6ab
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2023-12-24 00:17:45 +0300

    test submission form

--- client/stocklinx app/src/components/dataGrid/deployedProducts/UserDeployedProducts.tsx
+++ client/stocklinx app/src/components/dataGrid/deployedProducts/UserDeployedProducts.tsx
@@ -1,9 +1,10 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useColumns } from "./columns";
 import { RootState } from "../../../redux/rootReducer";
-import { useEffect } from "react";
-import Gridtable from "../../gridTable/GridTable";
+import { useEffect, useRef } from "react";
 import { deployedProductActions } from "../../../redux/deployedProduct/actions";
+import Gridtable from "../../gridTable/Gridtable";
+import { openSubmissionModal } from "../../../functions/exportSubmissionForm";
 
 interface UserDeployedProductsProps {
   userId: string;
@@ -15,6 +16,7 @@ const UserDeployedProducts: React.FC<UserDeployedProductsProps> = ({
   const deployedProducts = useSelector(
     (state: RootState) => state.deployedProduct.deployedProducts
   );
+  const gridtableRef: any = useRef();
 
   const refreshData = () => {
     dispatch(deployedProductActions.getAll());
@@ -25,11 +27,43 @@ const UserDeployedProducts: React.FC<UserDeployedProductsProps> = ({
   }, [userId]);
 
   return (
-    <Gridtable
-      itemKey="id"
-      data={deployedProducts.filter((dp) => dp.userId === userId)}
-      columns={useColumns()}
-    />
+    <>
+      <Gridtable
+        itemKey="id"
+        ref={gridtableRef}
+        data={deployedProducts.filter((dp) => dp.userId === userId)}
+        columns={useColumns()}
+        enableSelectActions
+      />
+      <button
+        onClick={() =>
+          openSubmissionModal({
+            userFullName: "İzzet Çakar",
+            companyName: "Bilge Adam",
+            branchName: "Bilgi İşlem",
+            department: "Yazılım",
+            userStartDate: "2021-05-05",
+            userTitle: "Yazılım Uzmanı",
+            products: [
+              {
+                category: "Bilgisayar",
+                title: "Lenovo",
+                description: "Lenovo Thinkpad",
+              },
+              {
+                category: "Bilgisayar",
+                title: "Lenovo",
+                description: "Lenovo Thinkpad",
+              },
+            ],
+            assignDate: "2023-12-23T18:24:45.061183Z",
+            delivererFullName: "Teslimat İzzet Çakarrrrrrrrrrrrrrrrrrrrrrrr",
+          })
+        }
+      >
+        show
+      </button>
+    </>
   );
 };
 
