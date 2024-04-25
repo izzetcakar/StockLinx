3b01c4ec37074a44485cb52d65495418aba43f69
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2024-04-25 21:03:21 +0300

    Create ProductCheckInDto.cs

---
+++ server/StockLinx.Core/DTOs/Others/ProductCheckInDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockLinx.Core.DTOs.Others
+{
+    public class ProductCheckInDto
+    {
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime AssignDate { get; set; }
+        public int Quantity { get; set; }
+        public string? Notes { get; set; }
+    }
+}
