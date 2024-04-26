ad54e747840bdee96b40de6e1a0baeef1e76c3eb
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2024-04-27 01:15:31 +0300

    redux devtools extension is applied

--- client/stocklinx app/src/redux/store.ts
+++ client/stocklinx app/src/redux/store.ts
@@ -2,11 +2,16 @@ import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./rootSaga";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
 
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 sagaMiddleware.run(rootSaga);
 
