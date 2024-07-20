dc8212c9f5c78ccb98a28bcdc776614c3faee5f4
İzzet Çakar <93754343+izzetcakar@users.noreply.github.com>
2024-07-20 03:20:27 +0300

    Separated columns and fixed availablequantity bug

--- client/stocklinx app/src/pages/accessory/Accessories.tsx
+++ client/stocklinx app/src/pages/accessory/Accessories.tsx
@@ -1,80 +1,66 @@
-import { useMemo } from 'react';
-import {
-  MantineReactTable,
-  useMantineReactTable,
-  type MRT_ColumnDef,
-} from 'mantine-react-table';
-import { ActionIcon, Loader, Menu, Tooltip  } from '@mantine/core';
-import { IconUserCircle, IconSend, IconRefresh } from '@tabler/icons-react';
-import { useAccessory, useCompany } from '@/hooks/query';
-import { IAccessory } from '@/interfaces/serverInterfaces';
-import { EntityCells } from '@/cells/Entity';
+import { MantineReactTable, useMantineReactTable } from "mantine-react-table";
+import { ActionIcon, Menu, Tooltip } from "@mantine/core";
+import { IconSend, IconRefresh, IconEdit } from "@tabler/icons-react";
+import { useAccessory } from "@/hooks/query";
+import { IAccessory } from "@/interfaces/serverInterfaces";
+import { useColumns } from "./columns";
+import { openAccessoryModal } from "@/utils/modalUtils";
 
 const Accessories = () => {
-  const { data,isRefetching,refetch } = useAccessory.GetAll();
-  const { data: companies,isRefetching:loading,refetch:getcom } = useCompany.Lookup();
-
-  const columns = useMemo<MRT_ColumnDef<IAccessory>[]>(
-    () => [
-      {
-        accessorKey: 'companyId',
-        header: 'Company',
-        Cell: ({ renderedCellValue }) => EntityCells.Company(renderedCellValue as string),
-        filterVariant: 'select',
-        mantineFilterSelectProps: () => ({
-          data: loading ? [] : companies,
-          onDropdownOpen: getcom,
-          rightSection: loading && <Loader size={16} />
-        }),
-      },
-      {
-        accessorKey: "tag",
-        header: "Tag",
-      },
-      {
-        accessorKey: 'name',
-        header: 'Name',
-      }
-    ],
-    [loading, companies]
-  );
-  
+  const { columns } = useColumns();
+  const { data, isRefetching, refetch } = useAccessory.GetAll();
 
   const table = useMantineReactTable<IAccessory>({
     columns,
-    data : data || [],
-    enableGlobalFilter:false,
-    enableDensityToggle:false,
+    data: data || [],
+    enableGlobalFilter: false,
+    enableDensityToggle: false,
     enableColumnOrdering: true,
     enableFacetedValues: true,
     enableRowActions: true,
     enableRowSelection: true,
     state: {
-      showLoadingOverlay:isRefetching,
-      density: 'xs',
+      showLoadingOverlay: isRefetching,
+      density: "xs",
+      isLoading: isRefetching,
     },
-    paginationDisplayMode: 'pages',
-    positionToolbarAlertBanner: 'bottom',
+    paginationDisplayMode: "pages",
+    positionToolbarAlertBanner: "bottom",
     mantinePaginationProps: {
-      radius: 'md',
-      size: 'md',
+      radius: "md",
+      size: "md",
     },
     renderTopToolbarCustomActions: () => (
       <Tooltip label="Refresh Data">
-        <ActionIcon onClick={() => refetch()} variant='subtle' color='black'>
+        <ActionIcon onClick={() => refetch()} variant="subtle" color="black">
           <IconRefresh />
         </ActionIcon>
       </Tooltip>
     ),
-    renderRowActionMenuItems: () => (
+    renderRowActionMenuItems: ({ row }) => (
       <>
-        <Menu.Item leftSection={<IconUserCircle />}>View Profile</Menu.Item>
-        <Menu.Item leftSection={<IconSend />}>Send Email</Menu.Item>
+        <Menu.Item
+          onClick={() => openAccessoryModal(row.original)}
+          leftSection={<IconEdit />}
+        >
+          Edit
+        </Menu.Item>
+        <Menu.Item
+          onClick={() => console.log(row.original)}
+          leftSection={<IconSend />}
+        >
+          Show
+        </Menu.Item>
       </>
     ),
   });
 
-  return <MantineReactTable table={table}/>;
+  return (
+    <>
+      <button onClick={() => console.log(data)}>show</button>
+      <MantineReactTable table={table} />
+    </>
+  );
 };
 
 export default Accessories;
--- client/stocklinx app/src/pages/accessory/columns.tsx
+++ client/stocklinx app/src/pages/accessory/columns.tsx
@@ -1,12 +1,45 @@
-import { IAccessory } from "@interfaces/serverInterfaces";
-import { useAccessory } from "@queryhooks";
+import { IAccessory, IEmployeeProduct } from "@interfaces/serverInterfaces";
+import { useAccessory, useCompany } from "@queryhooks";
 import { EntityCardColumn } from "@/interfaces/clientInterfaces";
+import { MRT_ColumnDef } from "mantine-react-table";
+import { EntityCells } from "@/cells/Entity";
+import { Button, Loader } from "@mantine/core";
+import { openCheckInModal } from "@/utils/modalUtils";
+import { useInitial } from "@/hooks/initial/useInitial";
 import AccessoryForm from "@/forms/accessory/AccessoryForm";
 import HistoryLogs from "@/components/dataGrid/customLog/HistoryLogs";
 import EmployeeProductSeats from "@/components/dataGrid/productseats/EmployeeProductSeats";
 
 export const useColumns = () => {
+  const initial = useInitial();
+  const {
+    data: companies,
+    isRefetching: loading,
+    refetch: getcom,
+  } = useCompany.Lookup();
+  const { mutate: checkIn } = useAccessory.CheckIn();
   const { mutate: checkOut } = useAccessory.CheckOut();
+
+  const onCheckInHandler = (data: IEmployeeProduct) => {
+    checkIn({
+      productId: data.accessoryId as string,
+      employeeId: data.employeeId,
+      assaignDate: data.assignDate,
+      notes: data.notes,
+      quantity: data.quantity,
+    });
+  };
+
+  const onHeadToModal = (accessory: IAccessory) => {
+    const newEmployeeProduct = initial.EmployeeProduct;
+    newEmployeeProduct.accessoryId = accessory.id;
+    openCheckInModal(
+      accessory.companyId,
+      ["Employee"],
+      newEmployeeProduct,
+      onCheckInHandler
+    );
+  };
 
   const cardColumns: EntityCardColumn[] = [
     {
@@ -39,6 +72,53 @@ export const useColumns = () => {
     {
       title: "History",
       renderData: (e) => <HistoryLogs id={e.id} />,
+    },
+  ];
+
+  const columns: MRT_ColumnDef<IAccessory>[] = [
+    {
+      accessorKey: "companyId",
+      header: "Company",
+      Cell: ({ renderedCellValue }) =>
+        EntityCells.Company(renderedCellValue as string),
+      filterVariant: "select",
+      mantineFilterSelectProps: () => ({
+        data: loading ? [] : companies,
+        onDropdownOpen: getcom,
+        rightSection: loading && <Loader size={16} />,
+      }),
+    },
+    {
+      accessorKey: "tag",
+      header: "Tag",
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+    },
+    {
+      accessorKey: "availableQuantity",
+      header: "Available Quantity",
+      Cell: ({ row }) => row.original.availableQuantity || 0,
+    },
+    {
+      header: "Check In",
+      Cell: ({ row }) => (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            color={"green"}
+            variant="filled"
+            size="xs"
+            disabled={
+              row.original.availableQuantity !== undefined &&
+              row.original?.availableQuantity < 1
+            }
+            onClick={() => onHeadToModal(row.original)}
+          >
+            Check In
+          </Button>
+        </div>
+      ),
     },
   ];
 
@@ -142,8 +222,6 @@ export const useColumns = () => {
   //     visible: false,
   //   },
   // ];
-
-  const columns: any = [];
 
   return { columns, cardColumns };
 };
